1f56e2092a135eb18e00dc3dfeec2557f2befacf
gramkin <bakluzhino@gmail.com>
2024-01-28 22:50:28 +0100

    fix

---
+++ lib/ui/src/lib/components/GeneFeatureDropdown/index.vue
@@ -0,0 +1,19 @@
+<script lang="ts" setup>
+import SelectInput from '../SelectInput.vue';
+import { getOptions } from './data';
+
+defineEmits(['update:modelValue']);
+
+defineProps<{
+  modelValue: string;
+}>();
+
+const options = getOptions().map((o) => ({
+  text: o.label,
+  value: o.code,
+}));
+</script>
+
+<template>
+  <select-input label="Gene Feature" :model-value="modelValue" :options="options" @update:model-value="$emit('update:modelValue', $event)" />
+</template>
