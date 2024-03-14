9e397e6088da3e8a3e8e04a6ee21d0a5062d004a
gramkin <bakluzhino@gmail.com>
2024-03-14 02:51:22 +0100

    pl-uikit: microfix

--- lib/ui/src/lib/components/GridTable/types.ts
+++ lib/ui/src/lib/components/GridTable/types.ts
@@ -22,7 +22,7 @@ export type ColumnSettings = {
   valueType?: ValueType;
 };
 
-export type ShowContextOptions = <T = string>(options: Option<T>[], onSelect: (op: T) => void) => void;
+export type ShowContextOptions = <T extends string = string>(options: Option<T>[], onSelect: (op: T) => void) => void;
 
 export type ColumnEvent = 'delete:column' | 'expand:column';
 
