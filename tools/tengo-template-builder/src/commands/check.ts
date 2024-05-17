ab26fdeacde9d3313da6c1be09b6f0509b379e15
Korenevskiy Denis <denkoren@gmail.com>
2024-05-17 03:32:56 +0200

    fix: return exit code from spawned tgo-test

--- tools/tengo-template-builder/src/commands/check.ts
+++ tools/tengo-template-builder/src/commands/check.ts
@@ -1,8 +1,7 @@
-import { spawn } from 'child_process';
-import { Command, Args } from '@oclif/core'
+import { Command } from '@oclif/core'
 import { createLogger } from '../compiler/main'
 import { dumpAll } from '../shared/dump';
-import { FlagInput } from '@oclif/core/lib/interfaces/parser';
+import { spawnEmbed, waitFor } from '../shared/proc';
 
 export default class Check extends Command {
   static override description = 'check tengo sources for language processor an'
@@ -20,19 +19,13 @@ export default class Check extends Command {
   public async run(): Promise<void> {
     const testerArgs: string[] = (this.argv.length == 0) ? ['./src'] : this.argv
 
-    const tester = spawn(
-      'npx', ['tgo-test', 'check', '--artifacts', '-', ...testerArgs],
-      { stdio: ['pipe', process.stdout, process.stderr] });
-
-    tester.stdin.on('error', (err: any) => {
-      if (err.code === 'EPIPE') {
-        // ignore EPIPE error as it stands for broken command run. 
-        // The command will write normal problem description by itself.
-      }
-    });
+    const tester = spawnEmbed('npx', 'tgo-test', 'check', '--artifacts', '-', ...testerArgs)
 
     const logger = createLogger('error');
     dumpAll(logger, tester.stdin)
     tester.stdin.end()
+
+    const code = await waitFor(tester)
+    process.exit(code)
   }
 }
--- tools/tengo-template-builder/src/commands/test.ts
+++ tools/tengo-template-builder/src/commands/test.ts
@@ -1,7 +1,7 @@
-import { spawn } from 'child_process';
 import { Command } from '@oclif/core'
 import { createLogger } from '../compiler/main'
 import { dumpAll } from '../shared/dump';
+import { spawnEmbed, waitFor } from '../shared/proc';
 
 export default class Test extends Command {
   static override description = 'run tengo unit tests (.test.tengo)'
@@ -15,19 +15,13 @@ export default class Test extends Command {
   public async run(): Promise<void> {
     const testerArgs: string[] = (this.argv.length == 0) ? ['./src'] : this.argv
 
-    const tester = spawn(
-      'npx', ['tgo-test', 'run', '--artifacts', '-', ...testerArgs],
-      { stdio: ['pipe', process.stdout, process.stderr] });
-
-    tester.stdin.on('error', (err: any) => {
-      if (err.code === 'EPIPE') {
-        // ignore EPIPE error as it stands for broken command run. 
-        // The command will write normal problem description by itself.
-      }
-    });
+    const tester = spawnEmbed('npx', 'tgo-test', 'run', '--artifacts', '-', ...testerArgs)
 
     const logger = createLogger('error');
     dumpAll(logger, tester.stdin)
     tester.stdin.end()
+
+    const code = await waitFor(tester)
+    process.exit(code)
   }
 }
