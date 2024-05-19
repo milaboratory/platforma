91e63cd97f8701808bc2912f6c0fd52eb12529e5
gramkin <bakluzhino@gmail.com>
2024-05-20 01:08:15 +0200

    update pl core version

--- lib/ui/src/main.ts
+++ lib/ui/src/main.ts
@@ -4,4 +4,6 @@ import './lib/assets/ui.scss';
 import './demo/demo.scss';
 import DemoApp from '@/demo/App.vue';
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore @TODO (after vue update)
 createApp(DemoApp).use(router).mount('#app');
