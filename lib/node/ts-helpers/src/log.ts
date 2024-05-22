c84cd2f43357b6fdcc9cb001ffcfa81617857cfc
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2024-05-22 09:55:01 +0300

    logging facade split

--- lib/node/ts-helpers/src/log.ts
+++ lib/node/ts-helpers/src/log.ts
@@ -1,8 +1,3 @@
-// @ts-ignore
-import type { Command } from '@oclif/core';
-// @ts-ignore
-import type { Logger } from 'winston';
-
 /** Minimalistic logger facade */
 export interface MiLogger {
   info(msg: string): void;
@@ -26,39 +21,5 @@ export class ConsoleLoggerAdapter implements MiLogger {
 
   error(msg: string | Error): void {
     this.console.error(msg);
-  }
-}
-
-export class OclifLoggerAdapter implements MiLogger {
-  constructor(private readonly cmd: Command) {
-  }
-
-  info(msg: string): void {
-    this.cmd.log(msg);
-  }
-
-  warn(msg: string | Error): void {
-    this.cmd.warn(msg);
-  }
-
-  error(msg: string | Error): void {
-    this.cmd.error(msg);
-  }
-}
-
-export class WinstonLoggerAdapter implements MiLogger {
-  constructor(private readonly logger: Logger) {
-  }
-
-  info(msg: string): void {
-    this.logger.info(msg);
-  }
-
-  warn(msg: string | Error): void {
-    this.logger.warn(msg);
-  }
-
-  error(msg: string | Error): void {
-    this.logger.error(msg);
   }
 }
