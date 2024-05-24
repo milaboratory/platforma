d4af50029bd294b15021f9d6a07c14c230e89b2f
Gleb Zakharov <snyssfx@posteo.net>
2024-05-24 10:00:44 +0200

    add rTypeToMetadata grpc helper for grpc drivers

--- lib/node/pl-client/src/driver.ts
+++ lib/node/pl-client/src/driver.ts
@@ -1,7 +1,8 @@
-import { LLPlClient } from './ll_client';
 import { PlClient } from './client';
 import { GrpcTransport } from '@protobuf-ts/grpc-transport';
+import type { RpcOptions } from '@protobuf-ts/runtime-rpc';
 import { Dispatcher } from 'undici';
+import { ResourceType } from './types';
 
 /** Drivers must implement this interface */
 export interface PlDriver {
@@ -15,4 +16,15 @@ export interface PlDriverDefinition<Drv extends PlDriver> {
 
   /** Initialization routine, will be executed only once for each driver in a specific client */
   init(pl: PlClient, grpcTransport: GrpcTransport, httpDispatcher: Dispatcher): Drv;
+}
+
+// addRTypeToMetadata adds a metadata with resource type
+// for every RPC call. It is necessary for the platform core
+// to proxy the call to the proper controller.
+export function addRTypeToMetadata(rType: ResourceType, options?: RpcOptions) {
+  options = options ?? {};
+  options.meta = options.meta ?? {};
+  options.meta['resourceType'] = `${rType.name}:${rType.version}`;
+
+  return options;
 }
