f0dc4a8ee30faf010cf043a7c1cbf85a1cffed09
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2024-06-21 11:59:12 +0300

    feat: rename to sdk-model feat: blob driver API

--- lib/model/common/src/block_state.ts
+++ lib/model/common/src/block_state.ts
@@ -1,20 +1,24 @@
 import { AuthorMarker } from './author_marker';
 
-/** *
- * @argument
+/**
+ * @param Args    sets type of block arguments passed to the workflow
+ * @param Outputs type of the outputs returned by the workflow and rendered
+ *                according to the output configuration specified for the block
+ * @param UiState data that stores only UI related state, that is not passed
+ *                to the workflow
  */
 export interface BlockState<Args, Outputs extends Record<string, unknown>, UiState> {
-  /** Author marker for Args and UI state. Absence of author marker, means that
+  /** Author marker for Args and UI State. Absence of author marker, means that
    * last modifier of the state provided no author marker. */
   readonly author?: AuthorMarker;
 
-  /** Block arguments */
+  /** Block arguments passed to the workflow */
   readonly args: Args;
 
-  /** UI State, i.e. any block state that must be persisted but is not passed
-   * to the backend template. */
-  readonly ui: UiState;
-
-  /** Outputs, rendered with block-specified config. */
+  /** Outputs rendered with block config */
   readonly outputs: Outputs;
+
+  /** UI State persisted in the block state but not passed to the backend
+   * template */
+  readonly ui: UiState;
 }
---
+++ lib/model/common/src/drivers/blob.ts
@@ -0,0 +1,40 @@
+import { Branded } from '../branding';
+
+/** Handle of locally downloaded blob. This handle is issued only after the
+ * blob's content is downloaded locally, and ready for quick access. */
+export type LocalBlobHandle = Branded<string, 'LocalBlobHandle'>
+
+/** Handle of remote blob. This handle is issued as soon as the data becomes
+ * available on the remote server. */
+export type RemoteBlobHandle = Branded<string, 'RemoteBlobHandle'>
+
+/** Being configured inside the output structure provides information about
+ * blob's content and means to retrieve it when needed. */
+export interface BlobHandleAndSize<
+  H extends LocalBlobHandle | RemoteBlobHandle = LocalBlobHandle | RemoteBlobHandle
+> {
+  /** Handle to retrieve block content using {@link BlobDriver.getContent()} */
+  readonly handle: H;
+
+  /** Blob size in bytes. */
+  readonly size: number;
+}
+
+/** Being configured inside the output structure provides information about
+ * locally downloaded blob and means to retrieve it's content when needed. This
+ * structure is created only after the blob's content is downloaded locally, and
+ * ready for quick access. */
+export type LocalBlobHandleAndSize = BlobHandleAndSize<LocalBlobHandle>;
+
+/** Being configured inside the output structure provides information about
+ * remote blob and means to retrieve it's content when needed. This structure
+ * is created as soon as remote blob becomes available. */
+export type RemoteBlobHandleAndSize = BlobHandleAndSize<RemoteBlobHandle>;
+
+/** Defines API of blob driver as it is seen from the block UI code. */
+export interface BlobDriver {
+  /** Given the blob handle returns its content. Depending on the handle type,
+   * content will be served from locally downloaded file, or directly from
+   * remote platforma storage. */
+  getContent(handle: LocalBlobHandle | RemoteBlobHandle): Promise<Uint8Array>;
+}
