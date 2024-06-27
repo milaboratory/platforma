579605dd9bc045e84a14aab5254c8e9a6e962c9d
Gleb Zakharov <snyssfx@posteo.net>
2024-06-27 17:58:47 +0200

    unnest log structure

--- lib/model/common/src/drivers/log.ts
+++ lib/model/common/src/drivers/log.ts
@@ -3,11 +3,16 @@ export type AnyLogHandle = LiveLogHandle | ReadyLogHandle;
 export type LiveLogHandle = `log+live://log/${string}`;
 export type ReadyLogHandle = `log+ready://log/${string}`;
 
-export type StreamingApiResponse = {
-  data: Uint8Array;
-  size: bigint;
-  newOffset: bigint;
+export type StreamingApiResponse =
+  | {
+    data: Uint8Array;
+    size: bigint;
+    newOffset: bigint;
 
-  live: boolean;
-  shouldUpdateHandle: boolean;
-};
+    live: boolean;
+    shouldUpdateHandle: false;
+  }
+  | {
+    live: boolean;
+    shouldUpdateHandle: true;    
+  };
