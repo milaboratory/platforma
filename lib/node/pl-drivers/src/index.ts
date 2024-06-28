3e9fba5c7a69da8203246c9cfe36d7a343f6f648
Gleb Zakharov <snyssfx@posteo.net>
2024-06-28 11:46:45 +0200

    export logs driver

--- lib/node/pl-drivers/src/index.ts
+++ lib/node/pl-drivers/src/index.ts
@@ -8,5 +8,6 @@ export * from './clients/helpers';
 export * from './drivers/download_and_logs_blob';
 export * from './drivers/upload';
 export * from './drivers/logs_stream';
+export * from './drivers/logs';
 export * from './drivers/download_url';
 export * from './drivers/helpers';
