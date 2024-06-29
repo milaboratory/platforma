33e445885bc0dc62cc1f78102d0e58dc49f6c315
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2024-06-29 17:12:07 +0300

    feat: removed selectros

--- lib/model/common/src/drivers/pframe/index.ts
+++ lib/model/common/src/drivers/pframe/index.ts
@@ -2,7 +2,6 @@ export * from './column_filter';
 export * from './data';
 export * from './find_columns';
 export * from './pframe';
-export * from './selectors';
 export * from './spec';
 export * from './table';
 export * from './table_calculate';
