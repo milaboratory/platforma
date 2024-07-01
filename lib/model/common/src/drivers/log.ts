a7eb21118a65d3655f6673b8f54d91ce6511c152
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2024-07-01 11:57:09 +0300

    feat: logs driver

--- lib/model/common/src/drivers/log.ts
+++ lib/model/common/src/drivers/log.ts
@@ -3,16 +3,37 @@ export type AnyLogHandle = LiveLogHandle | ReadyLogHandle;
 export type LiveLogHandle = `log+live://log/${string}`;
 export type ReadyLogHandle = `log+ready://log/${string}`;
 
-export type StreamingApiResponse =
-  | {
-    data: Uint8Array;
-    size: bigint;
-    newOffset: bigint;
+export type StreamingApiResponseOk = {
+  shouldUpdateHandle: false;
 
-    live: boolean;
-    shouldUpdateHandle: false;
-  }
-  | {
-    live: boolean;
-    shouldUpdateHandle: true;    
-  };
+  live: boolean;
+
+  data: Uint8Array;
+  size: number;
+  newOffset: number;
+}
+
+export type StreamingApiResponseHandleOutdated = {
+  shouldUpdateHandle: true;
+}
+
+export type StreamingApiResponse =
+  | StreamingApiResponseOk
+  | StreamingApiResponseHandleOutdated;
+
+/** Driver to retrieve logs given log handle */
+export interface LogsDriver {
+  lastLines(
+    handle: AnyLogHandle,
+    lineCount: number,
+    offsetBytes: number, // if -1, then start from the end
+    searchStr?: string
+  ): Promise<StreamingApiResponse>;
+
+  readText(
+    handle: AnyLogHandle,
+    lineCount: number,
+    offsetBytes: number, // if 0, then start from the beginning.
+    searchStr?: string
+  ): Promise<StreamingApiResponse>;
+}
