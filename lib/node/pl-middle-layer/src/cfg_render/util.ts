136e8f372c3fd01c3f6004573c5f73b538638e39
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2024-07-02 13:02:18 +0300

    wib before merge

--- lib/node/pl-middle-layer/src/cfg_render/util.ts
+++ lib/node/pl-middle-layer/src/cfg_render/util.ts
@@ -1,3 +1,6 @@
+import { Cfg } from '@milaboratory/sdk-ui';
+import { traverseCfg } from './traverse';
+
 export function mapRecord<T1, T2, Key extends string>(
   obj: Record<Key, T1>,
   mapper: (value: T1) => T2
@@ -8,7 +11,22 @@ export function mapRecord<T1, T2, Key extends string>(
   return result;
 }
 
-
 export function ifNotUndef<T, R>(value: T | undefined, cb: (value: T) => R): R | undefined {
   return value === undefined ? undefined : cb(value);
+}
+
+function isActiveCfg(cfg: Cfg): boolean {
+  switch (cfg.type) {
+    case 'GetUploadBlob':
+      return true;
+    default:
+      return false;
+  }
+}
+
+export function hasActiveCfgComponents(cfg: Cfg): boolean {
+  for (const nested of traverseCfg(cfg))
+    if (isActiveCfg(nested))
+      return true;
+  return false;
 }
---
+++ lib/node/pl-middle-layer/src/middle_layer/active_cfg.ts
@@ -0,0 +1,48 @@
+import { notEmpty } from '@milaboratory/ts-helpers';
+import { PlTreeEntry } from '@milaboratory/pl-tree';
+import { MiddleLayerEnvironment } from './middle_layer';
+import { Computable } from '@milaboratory/computable';
+import { projectFieldName, ProjectStructure, ProjectStructureKey } from '../model/project_model';
+import { allBlocks } from '../model/project_model_util';
+import { Pl } from '@milaboratory/pl-client-v2';
+import { BlockPackInfo } from '../model/block_pack';
+import { hasActiveCfgComponents, ifNotUndef } from '../cfg_render/util';
+import { normalizeBlockConfig } from '@milaboratory/sdk-ui';
+import { constructBlockContext } from './block_ctx';
+import { computableFromCfg } from '../cfg_render/executor';
+
+/** Returns derived general project state form the project resource */
+export function activeConfigs(prjEntry: PlTreeEntry, env: MiddleLayerEnvironment): Computable<unknown[]> {
+  return Computable.make(ctx => {
+    const prj = ctx.accessor(prjEntry).node();
+
+    const structure = notEmpty(prj.getKeyValueAsJson<ProjectStructure>(ProjectStructureKey));
+    const ret: Computable<unknown>[] = [];
+    for (const { id, renderingMode } of allBlocks(structure)) {
+      // block-pack
+      const blockPack = prj.traverse(
+        { field: projectFieldName(id, 'blockPack'), assertFieldType: 'Dynamic', errorIfFieldNotAssigned: true },
+        { field: Pl.HolderRefField, assertFieldType: 'Input', errorIfFieldNotFound: true }
+      );
+
+      const bpInfo = blockPack?.getDataAsJson<BlockPackInfo>();
+      if (bpInfo?.config === undefined)
+        continue;
+
+      const blockConf = normalizeBlockConfig(bpInfo.config);
+      const activeOutputConfigs = Object.entries(blockConf.outputs)
+        .map(([, cfg]) => cfg)
+        .filter(cfg => hasActiveCfgComponents(cfg));
+
+      if (activeOutputConfigs.length === 0)
+        continue;
+
+      const blockCtx = constructBlockContext(prj, id);
+
+      for (const cfg of activeOutputConfigs)
+        ret.push(computableFromCfg(env.drivers, blockCtx, cfg));
+    }
+
+    return ret;
+  });
+}
