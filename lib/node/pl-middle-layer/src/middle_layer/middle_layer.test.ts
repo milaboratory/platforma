c700c6d17c1ef12255010aa7141753f9f7d8e314
Gleb Zakharov <snyssfx@posteo.net>
2024-07-03 18:51:51 +0200

    temporarly skip upload-file

--- lib/node/pl-middle-layer/src/middle_layer/middle_layer.test.ts
+++ lib/node/pl-middle-layer/src/middle_layer/middle_layer.test.ts
@@ -433,7 +433,7 @@ test('should create download-file block, render it and gets outputs from its con
   });
 });
 
-test('should create upload-file block, render it and upload a file to pl server', async () => {
+test.skip('should create upload-file block, render it and upload a file to pl server', async () => {
   await withMl(async ml => {
     const pRid1 = await ml.createProject({ label: 'Project 1' }, 'id1');
     await ml.openProject(pRid1);
