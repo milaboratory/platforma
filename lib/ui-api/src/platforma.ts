626193d87c9fa4bf9a9b170ab2465ddd83bb72ec
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2024-07-13 13:36:40 +0300

    feat: Platforma now extends driver fields from DriverKit

--- lib/ui-api/src/platforma.ts
+++ lib/ui-api/src/platforma.ts
@@ -1,29 +1,20 @@
 import { BlockApi } from './block_api';
-import { BlobDriver, LogsDriver, LsDriver, ValueOrErrors } from '@milaboratory/sdk-model';
+import {
+  BlobDriver,
+  DriverKit,
+  LogsDriver,
+  LsDriver,
+  ValueOrErrors
+} from '@milaboratory/sdk-model';
 import { BlockConfig } from './builder';
 
 /** Defines all methods to interact with the platform environment from within a block UI. */
 export interface Platforma<
   Args = unknown,
   Outputs extends Record<string, ValueOrErrors<unknown>> = Record<string, ValueOrErrors<unknown>>,
-  UiState = unknown>
-  extends BlockApi<Args, Outputs, UiState> {
-  /** Driver allowing to retrieve blob data */
-  blobDriver: BlobDriver;
-
-  /** Driver allowing to retrieve blob data */
-  logDriver: LogsDriver;
-
-  /**
-   * Driver allowing to list local and remote files that current user has
-   * access to.
-   *
-   * Along with file listing this driver provides import handles for listed
-   * files, that can be communicated to the workflow via block arguments,
-   * converted into blobs using standard workflow library functions.
-   * */
-  lsDriver: LsDriver;
-}
+  UiState = unknown
+> extends BlockApi<Args, Outputs, UiState>,
+    DriverKit {}
 
 export type InferOutputsType<Pl extends Platforma> =
   Pl extends Platforma<unknown, infer Outputs> ? Outputs : never;
