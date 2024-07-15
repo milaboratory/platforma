18cacbc445fee143d4c1cf1335f3512af834d2cc
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2024-07-15 12:12:15 +0300

    feat: ResultCollection

--- lib/model/common/src/pool/entry.ts
+++ lib/model/common/src/pool/entry.ts
@@ -9,3 +9,12 @@ export type ResultPoolEntry<Data> = {
   /** PObject itself */
   readonly obj: PObject<Data>;
 };
+
+/** Collection of results from the result pool */
+export type ResultCollection<Data> = {
+  /** List of results from the pool */
+  readonly entries: ResultPoolEntry<Data>[];
+
+  /** False means that current collection is not fully loaded due to some computations still taking place */
+  readonly isComplete: boolean;
+};
