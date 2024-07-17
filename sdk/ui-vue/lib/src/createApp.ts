cfdeac7a2a62aa3f3bdaccd190f9c7a0649919d6
gramkin <bakluzhino@gmail.com>
2024-07-18 01:22:59 +0200

    wip (v0.0.2)

--- sdk/ui-vue/lib/src/createApp.ts
+++ sdk/ui-vue/lib/src/createApp.ts
@@ -1,27 +1,13 @@
 import { deepClone, setProp } from '@milaboratory/helpers/objects';
-import type { BlockOutputsBase, BlockState, Platforma } from '@milaboratory/sdk-ui';
+import { type BlockOutputsBase, type BlockState, type Platforma } from '@milaboratory/sdk-ui';
 import type { UnwrapRef, DeepReadonly } from 'vue';
-import { reactive, nextTick, computed, ref } from 'vue';
-import { ZodError } from 'zod';
-import type { UnwrapValueOrErrors, ModelOptions, Model } from './types';
-
-const identity = <T, V = T>(v: T): V => v as unknown as V;
-
-const ensureError = (cause: unknown) => {
-  if (cause instanceof Error) {
-    return cause;
-  }
-
-  return Error(String(cause));
-};
-
-const isZodError = (err: Error): err is ZodError => {
-  return err.name === 'ZodError';
-};
+import { reactive, nextTick } from 'vue';
+import type { UnwrapValueOrErrors, Routes, LocalState } from './types';
 
 export function createApp<Args = unknown, Outputs extends BlockOutputsBase = BlockOutputsBase, UiState = unknown>(
   state: BlockState<Args, Outputs, UiState>,
   platforma: Platforma<Args, Outputs, UiState>,
+  cb: () => LocalState,
 ) {
   type ReadonlyArgs = DeepReadonly<Args>;
 
@@ -29,67 +15,11 @@ export function createApp<Args = unknown, Outputs extends BlockOutputsBase = Blo
     args: state.args as ReadonlyArgs,
     outputs: state.outputs,
     ui: state.ui,
+    navigationState: {
+      href: '/',
+    },
     cloneArgs() {
       return deepClone(this.args) as Args;
-    },
-    createModel<M, V = unknown>(options: ModelOptions<M, V>): Model<M> {
-      const validate = options.validate ?? identity;
-
-      const { autoSave } = options;
-
-      const error = ref<Error | undefined>();
-
-      const localValue = ref<M>();
-
-      const save = () => {
-        if (localValue.value) {
-          options.onSave(validate(localValue.value));
-        }
-      };
-
-      const modelValue = computed({
-        get: () => {
-          if (localValue.value !== undefined) {
-            return localValue.value;
-          }
-
-          return options.get();
-        },
-        set(v) {
-          localValue.value = v;
-          error.value = undefined;
-          console.log('flush error');
-          try {
-            validate(v);
-            if (autoSave) {
-              save();
-            }
-          } catch (cause: unknown) {
-            const err = ensureError(cause);
-            console.log('err instanceof ZodError', err instanceof ZodError);
-            if (isZodError(err)) {
-              error.value = Error(err.format()._errors.join(',')); // @todo temp
-            } else {
-              error.value = err as Error; // @todo ensureError
-            }
-          }
-        },
-      });
-
-      const valid = computed(() => !error.value);
-
-      const isChanged = computed(() => localValue.value !== undefined);
-
-      const errorString = computed(() => (error.value ? error.value.message : ''));
-
-      return reactive({
-        modelValue,
-        valid,
-        isChanged,
-        error,
-        errorString,
-        save,
-      });
     },
     getOutputField(key: keyof Outputs) {
       return this.outputs[key];
@@ -140,5 +70,5 @@ export function createApp<Args = unknown, Outputs extends BlockOutputsBase = Blo
     await nextTick(); // @todo remove
   });
 
-  return app;
+  return Object.assign(app, cb());
 }
--- sdk/ui-vue/lib/src/types.ts
+++ sdk/ui-vue/lib/src/types.ts
@@ -1,11 +1,13 @@
 import type { ValueOrErrors } from '@milaboratory/sdk-ui';
-import type { ComputedGetter } from 'vue';
+import type { Component, ComputedGetter } from 'vue';
+import type { Expect, Equal } from '@milaboratory/helpers/types';
+import { z } from 'zod';
 
 export type UnwrapValueOrErrors<R extends ValueOrErrors<unknown>> = Extract<R, { ok: true }>['value'];
 
 export interface ModelOptions<T, V = T> extends ReadableComputed<T> {
   get(): T;
-  validate?(v: T): V;
+  validate?(v: unknown): V;
   onSave(v: V): void;
   autoSave?: boolean;
   // @todo debounce?: number
@@ -18,8 +20,53 @@ export type Model<T> = {
   error: Error | undefined;
   errorString: string | undefined;
   save: () => void;
+  revert: () => void;
 };
 
 interface ReadableComputed<T> {
   get: ComputedGetter<T>;
 }
+
+export type Routes = {
+  [key: string]: Component;
+};
+
+export type LocalState = {
+  routes: Routes;
+};
+
+/** Tests **/
+
+declare function __createModel<M, V = unknown>(options: ModelOptions<M, V>): Model<M>;
+
+declare const __args: {
+  seed: number;
+};
+
+const validate = z.coerce.number().int().parse;
+
+type _number = ReturnType<typeof validate>;
+
+const __model1 = __createModel({
+  get() {
+    return String(__args.seed);
+  },
+  validate,
+  autoSave: true,
+  onSave(seed) {
+    console.log('save', seed);
+  },
+});
+
+const __model2 = __createModel({
+  get() {
+    return __args.seed;
+  },
+  validate,
+  autoSave: true,
+  onSave(seed) {
+    console.log('save', seed);
+  },
+});
+
+type __cases = [Expect<Equal<Model<string>, typeof __model1>>, Expect<Equal<Model<number>, typeof __model2>>];
