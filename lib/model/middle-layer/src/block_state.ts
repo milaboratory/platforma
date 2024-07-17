0881265e52e8085c8dede61dc9a036aef15dd6cd
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2024-07-17 21:00:22 +0300

    feat: sdk model upgrade, Navigation state

--- lib/model/middle-layer/src/block_state.ts
+++ lib/model/middle-layer/src/block_state.ts
@@ -5,8 +5,10 @@ import { Optional } from 'utility-types';
 export type BlockStateInternal<
   Args = unknown,
   Outputs extends BlockOutputsBase = BlockOutputsBase,
-  UiState = unknown> = Optional<Readonly<BlockState<Args, Outputs, UiState>>, 'outputs'> & {
+  UiState = unknown,
+  Href extends string = string
+> = Optional<Readonly<BlockState<Args, Outputs, UiState, Href>>, 'outputs'> & {
   /** Author marker for Args and UI State. Absence of author marker, means that
    * last modifier of the state provided no author marker. */
   readonly author?: AuthorMarker;
-}
+};
