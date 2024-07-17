466ad8781f17edd40c68004cdccb85826494ff3b
Gleb Zakharov <snyssfx@posteo.net>
2024-07-17 10:42:10 +0200

    workflow: add initial workflow implementation (#2)
    * workflow: add initial workflow implementation
    
    * init
    
    * Builders & api re-work
    
    * fixed some PR comments
    
    * changes according to review comments
    
    * smart: fix buildUnlocked
    
    * build fixes, split map builder
    
    * chore: tengo-template-builder that supports ll.getTemplateId
    
    * changes according to review comments
    
    * test fixes
    
    * minors
    
    * comments and assert in tpl results
    
    * fix build
    
    ---------
    
    Co-authored-by: Stanislav Poslavsky <poslavskysv@milaboratories.com>
    Co-authored-by: Korenevskiy Denis <denkoren@gmail.com>

--- sdk/workflow-tengo/src/test/eph.test.ts
+++ sdk/workflow-tengo/src/test/eph.test.ts
@@ -17,21 +17,6 @@ tplTest("should return value when run simple ephemeral template", async ({ helpe
   expect(await mainResult.awaitStableValue()).eq("Truman Show");
 });
 
-tplTest("should set output when get outputs from output map in ephemeral template", async ({ helper, expect }) => {
-  const result = await helper.renderTemplate(
-    true,
-    "test.eph2",
-    ["main"],
-    (tx) => ({
-      input1: tx.createValue(
-        Pl.JsonObject,
-        JSON.stringify({ testValue: "Truman" })
-      ),
-    })
-  );
-  const mainResult = result.computeOutput("main", (a) => a?.getDataAsJson());
-  expect(await mainResult.awaitStableValue()).eq("Truman Show");
-});
 
 tplTest("should set output when not all inputs are ready", async ({ helper, expect }) => {
   const result = await helper.renderTemplate(
