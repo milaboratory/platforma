397e0d70c53e300ccffca20417f8c4aca8dfb9cd
nikolaygiman <gimanhead@gmail.com>
2024-08-09 17:54:44 +0200

    wip: added textarea and loader

---
+++ lib/ui/lib/src/components/Loader.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { computed } from 'vue';
+
+const props = withDefaults(
+  defineProps<{
+    loading: boolean;
+    progress: number;
+    completeMessage: string;
+  }>(),
+  { progress: 0, completeMessage: 'Completed' },
+);
+
+const readyMsg = computed(() => (props.progress === 100 ? props.completeMessage : ''));
+</script>
+
+<template>
+  <div v-if="loading" class="ui-loader">
+    <div class="ui-loader__indicator" :style="{ width: progress + '%' }"></div>
+    <div class="ui-loader__messages d-flex align-center pl-6 pr-6">
+      <div class="ui-loader__message flex-grow-1">{{ readyMsg }}</div>
+      <div class="ui-loader__percent">{{ progress + '%' }}</div>
+    </div>
+  </div>
+</template>
---
+++ lib/ui/lib/src/components/TextArea.vue
@@ -0,0 +1,88 @@
+<script lang="ts" setup>
+import { computed, ref, useSlots } from 'vue';
+import Tooltip from '@/components/Tooltip.vue';
+import DoubleContour from '@/utils/DoubleContour.vue';
+import { useLabelNotch } from '@/composition/useLabelNotch';
+
+const slots = useSlots();
+
+const emit = defineEmits(['update:modelValue']);
+
+const props = defineProps<{
+  modelValue?: string;
+  label?: string;
+  optional?: boolean;
+  error?: string;
+  helper?: string;
+  placeholder?: string;
+  disabled?: boolean;
+  dashed?: boolean;
+  rows?: number;
+  autogrow?: boolean;
+}>();
+
+const root = ref<HTMLInputElement | undefined>(undefined);
+const input = ref<HTMLTextAreaElement>();
+
+const value = computed({
+  get() {
+    return props.modelValue || '';
+  },
+  set(v) {
+    emit('update:modelValue', v);
+  },
+});
+
+const nonEmpty = computed(() => !!props.modelValue);
+
+function clear() {
+  emit('update:modelValue', undefined);
+}
+
+useLabelNotch(root);
+
+function adjustHeight() {
+  if (!props.autogrow) {
+    return;
+  }
+  const textarea = input.value;
+  if (textarea) {
+    textarea.style.height = 'auto'; // Reset height to auto
+    textarea.style.height = `${textarea.scrollHeight}px`; // Set height based on scrollHeight
+  }
+}
+</script>
+
+<template>
+  <div class="ui-text-area__envelope">
+    <div ref="root" class="ui-text-area" :class="{ optional, error, disabled, dashed, nonEmpty }">
+      <label v-if="label" ref="label">
+        {{ label }}
+        <span v-if="optional" style="opacity: 0.5">(optional)</span>
+        <tooltip v-if="slots.tooltip" class="info" position="top">
+          <template #tooltip>
+            <slot name="tooltip" />
+          </template>
+        </tooltip>
+      </label>
+      <textarea
+        ref="input"
+        v-model="value"
+        :rows="rows"
+        :disabled="disabled"
+        type="text"
+        :placeholder="placeholder || '...'"
+        spellcheck="false"
+        @input="adjustHeight"
+      />
+      <div class="ui-text-area__append">
+        <!-- clearable is not available for TextArea -->
+        <!-- <div v-if="clearable && nonEmpty" class="icon icon--clear" @click="clear" /> -->
+        <slot name="append" />
+      </div>
+      <double-contour class="ui-text-area__contour" />
+    </div>
+    <div v-if="error" class="ui-text-area__error">{{ error }}</div>
+    <div v-else-if="helper" class="ui-text-area__helper">{{ helper }}</div>
+  </div>
+</template>
