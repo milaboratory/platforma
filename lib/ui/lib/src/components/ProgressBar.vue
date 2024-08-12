6dd8cf5f7d0e165274fd53a5c9162df837f109ab
nikolaygiman <gimanhead@gmail.com>
2024-08-12 08:00:33 +0200

    fix: names

--- lib/ui/lib/src/components/ProgressBar.vue
+++ lib/ui/lib/src/components/ProgressBar.vue
@@ -14,11 +14,11 @@ const readyMsg = computed(() => (props.progress === 100 ? props.completeMessage 
 </script>
 
 <template>
-  <div v-if="loading" class="ui-loader">
-    <div class="ui-loader__indicator" :style="{ width: progress + '%' }"></div>
-    <div class="ui-loader__messages d-flex align-center pl-6 pr-6">
-      <div class="ui-loader__message flex-grow-1">{{ readyMsg }}</div>
-      <div class="ui-loader__percent">{{ progress + '%' }}</div>
+  <div v-if="loading" class="ui-progress-bar">
+    <div class="ui-progress-bar__indicator" :style="{ width: progress + '%' }"></div>
+    <div class="ui-progress-bar__messages d-flex align-center pl-6 pr-6">
+      <div class="ui-progress-bar__message flex-grow-1">{{ readyMsg }}</div>
+      <div class="ui-progress-bar__percent">{{ progress + '%' }}</div>
     </div>
   </div>
 </template>
