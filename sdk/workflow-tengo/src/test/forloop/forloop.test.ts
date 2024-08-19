a36ffebf28fbfa2baaed3d49effd1d3ee1282efb
Gleb Zakharov <snyssfx@posteo.net>
2024-08-19 18:24:00 +0200

    forloop: working test

--- sdk/workflow-tengo/src/test/forloop/forloop.test.ts
+++ sdk/workflow-tengo/src/test/forloop/forloop.test.ts
@@ -10,8 +10,8 @@ tplTest(
       (tx) => ({})
     );
     const concat = result.computeOutput('concat', (a, ctx) => {
-      const results = ["c1", "c2", "c3"].
-        map(fName => a?.getField(fName)?.value?.getDataAsJson<string>());
+      const fields = ["c1", "c2", "c3"];
+      const results = fields.map(fName => a?.getField(fName)?.value?.getDataAsJson<string>());
 
       if (results.includes(undefined))
         ctx.markUnstable();
@@ -19,9 +19,9 @@ tplTest(
       return results;
     });
 
-    const concatReversed = result.computeOutput('concat', (a, ctx) => {
-      const results = ["c1", "c2", "c3"].
-        map(fName => a?.getField(fName)?.value?.getDataAsJson<string>());
+    const concatReversed = result.computeOutput('concatReversed', (a, ctx) => {
+      const fields = ["c1", "c2", "c3"];
+      const results = fields.map(fName => a?.getField(fName)?.value?.getDataAsJson<string>());
 
       if (results.includes(undefined))
         ctx.markUnstable();
@@ -29,13 +29,13 @@ tplTest(
       return results;
     });
 
-    expect(await concat.awaitStableValue()).eq([
+    expect(await concat.awaitStableValue()).toEqual([
       "astring_bstring_c11",
       "astring_bstring_c22",
       "astring_bstring_c33",
     ]);
 
-    expect(await concatReversed.awaitStableValue()).eq([
+    expect(await concatReversed.awaitStableValue()).toEqual([
       "c11_bstring_astring",
       "c22_bstring_astring",
       "c33_bstring_astring",
