815a8cef8b43ec9db7ea1420d19656437ae4d9e1
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2024-08-19 13:21:30 +0200

    fix: proper dummy result generation for workflows with absent pre-run template in staging environment

--- sdk/workflow-tengo/src/test/workflow/workflow.test.ts
+++ sdk/workflow-tengo/src/test/workflow/workflow.test.ts
@@ -1,5 +1,6 @@
 import { Pl, type PlTransaction } from '@milaboratory/pl-middle-layer';
 import { tplTest } from '@milaboratory/sdk-test';
+import { read } from 'fs';
 
 function createObject(tx: PlTransaction, value: any) {
   return tx.createValue(Pl.JsonObject, JSON.stringify(value));
@@ -32,5 +33,34 @@ tplTest(
         .output('outputResult', (a) => a?.getDataAsJson())
         .awaitStableValue()
     ).eq('Truman PreRun Show Run');
+  }
+);
+
+tplTest(
+  'should return dummy result and ctx for staging if pre-run template not specified',
+  async ({ helper, expect }) => {
+    const prerun = await helper.renderWorkflow('test.workflow.wf2', true, {
+      testValue: 'Truman PreRun'
+    });
+
+    expect(
+      await prerun.renderResult
+        .computeOutput('context', (c, ctx) => {
+          const ready = c?.getIsReadyOrError();
+          if (!ready) ctx.markUnstable('not_ready');
+          return ready;
+        })
+        .awaitStableValue()
+    ).eq(true);
+
+    expect(
+      await prerun.renderResult
+        .computeOutput('result', (c, ctx) => {
+          const ready = c?.getIsReadyOrError();
+          if (!ready) ctx.markUnstable('not_ready');
+          return ready;
+        })
+        .awaitStableValue()
+    ).eq(true);
   }
 );
