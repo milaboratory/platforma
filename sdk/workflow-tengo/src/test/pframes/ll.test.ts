5e74bc9f17df5285775c83826a4b8da0dc291c88
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2024-08-21 19:33:43 +0200

    done

--- sdk/workflow-tengo/src/test/pframes/ll.test.ts
+++ sdk/workflow-tengo/src/test/pframes/ll.test.ts
@@ -1,12 +1,23 @@
-import { Pl, field, resourceType } from '@milaboratory/pl-middle-layer';
+import { AnyRef, Pl, field, resourceType } from '@milaboratory/pl-middle-layer';
 import { awaitStableState, tplTest } from '@milaboratory/sdk-test';
 
-tplTest(
-  'should correctly execute low level aggregation routine',
+tplTest.for([
+  { name: '[0]', indices: [0], expectedResult: { '[1]': 1, '[2]': 1 } },
+  { name: '[1]', indices: [1], expectedResult: { '[1]': 2 } },
+  { name: '[]', indices: [], expectedResult: { '[1,1]': 1, '[1,2]': 1 } },
+  { name: '[0,1]', indices: [0, 1], expectedResult: { '[]': 2 } },
+  {
+    name: '[0]+3',
+    indices: [0],
+    expectedResult: { '[1]': 4, '[2]': 4 },
+    base: 3
+  }
+])(
+  'should correctly execute low level aggregation routine $name',
   { timeout: 10000 },
-  async ({ helper, expect }) => {
+  async ({ indices, expectedResult, base }, { helper, expect }) => {
     const result = await helper.renderTemplate(
-      false,
+      true,
       'test.pframes.ll_agg_1',
       ['result'],
       (tx) => {
@@ -22,19 +33,35 @@ tplTest(
         tx.createField(field(data, '[1, 2]'), 'Input', v1);
         tx.lockInputs(data);
 
-        return {
+        const inputs: Record<string, AnyRef> = {
           params: tx.createValue(
             Pl.JsonObject,
             JSON.stringify({
-              indices: [0],
+              indices,
               eph: false
             })
           ),
           data: data
         };
+
+        if (base !== undefined) {
+          inputs['base'] = tx.createValue(Pl.JsonObject, JSON.stringify(base));
+        }
+
+        return inputs;
       }
     );
-    const r = result.computeOutput('result', (a) => a?.getDataAsJson());
-    console.dir(await awaitStableState(r));
+    const r = result.computeOutput('result', (a, ctx) => {
+      if (a === undefined) return undefined;
+      if (!a.getIsReadyOrError()) {
+        ctx.markUnstable('not_ready');
+        return undefined;
+      }
+      return Object.fromEntries(
+        a.listInputFields().map((f) => [f, a.traverse(f)!.getDataAsJson()])
+      );
+    });
+    const finalResult = await awaitStableState(r);
+    expect(finalResult).toStrictEqual(expectedResult);
   }
 );
