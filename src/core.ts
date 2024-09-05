d67f0d7b032efa572deffc4146c3c315cbae77f8
Gleb Zakharov <snyssfx@posteo.net>
2024-09-05 12:09:11 +0200

    license: merge main branch and fix all conflicts, bump ver

--- src/core.ts
+++ src/core.ts
@@ -54,15 +54,16 @@ export default class Core {
         )
 
         const storageDirs: string[] = [
-            `${configOptions.localRoot}/software`,
-            `${configOptions.localRoot}/software-local`,
-            `${configOptions.localRoot}/blocks/local`,
+            `${configOptions.localRoot}/packages`,
+            `${configOptions.localRoot}/packages-local`,
+            `${configOptions.localRoot}/blocks-local`,
         ]
         if (configOptions.storages.primary.type === 'FS') {
             storageDirs.push(configOptions.storages.primary.rootPath)
         }
         if (configOptions.storages.library.type === 'FS') {
             storageDirs.push(configOptions.storages.library.rootPath)
+            configOptions.hacks.libraryDownloadable = false
         }
         if (configOptions.storages.work.type === 'FS') {
             storageDirs.push(configOptions.storages.work.rootPath)
