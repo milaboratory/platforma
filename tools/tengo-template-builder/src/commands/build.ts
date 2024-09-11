e4218a175b6c1053f571e509a504c53906f83c38
Gleb Zakharov <snyssfx@posteo.net>
2024-09-11 14:17:51 +0200

    ctags support

--- tools/tengo-template-builder/src/commands/build.ts
+++ tools/tengo-template-builder/src/commands/build.ts
@@ -1,7 +1,14 @@
+import {
+  SpawnSyncReturns,
+  spawnSync
+} from 'child_process';
 import { Command } from '@oclif/core';
 import { compile, savePacks, createLogger } from '../compiler/main';
-import { GlobalFlags } from '../shared/basecmd';
+import { CtagsFlags, GlobalFlags } from '../shared/basecmd';
 import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+import * as nodedir from 'node-dir';
+import winston from 'winston';
 
 export default class Build extends Command {
   static override description =
@@ -9,7 +16,10 @@ export default class Build extends Command {
 
   static override examples = ['<%= config.bin %> <%= command.id %>'];
 
-  static override flags = { ...GlobalFlags };
+  static override flags = {
+    ...GlobalFlags,
+    ...CtagsFlags
+  };
 
   public async run(): Promise<void> {
     const { flags } = await this.parse(Build);
@@ -42,6 +52,78 @@ export default class Build extends Command {
     // savePacks(logger, compiledDev, 'dev')
     // logger.info('')
 
+    if (flags['generate-tags'])
+      checkAndGenerateCtags(logger, flags)
+
     logger.info('Template Pack build done.');
+  }
+}
+
+function checkAndGenerateCtags(logger: winston.Logger, flags: {
+  'tags-file': string,
+  'tags-root-dir': string,
+  'tags-additional-args': string[],
+}) {
+
+  const fileName = path.resolve(flags['tags-file']);
+  const rootDir = path.resolve(flags['tags-root-dir']);
+  const additionalArgs = flags['tags-additional-args'];
+  
+  nodedir.files(rootDir, (_, files) => {
+    // all tengo files in dirs and subdirs
+    const tengoFiles = files.filter(f => f.endsWith(".tengo"));
+    
+    logger.info(
+      `Generating tags for tengo autocompletion from "${rootDir}" \
+in "${fileName}", additional arguments: "${additionalArgs}".
+Found ${tengoFiles.length} tengo files...`)
+
+    // see https://docs.ctags.io/en/latest/man/ctags-optlib.7.html#perl-pod
+    const result = spawnSync(
+      'ctags',
+      [
+        "-f", fileName, ...additionalArgs,
+        "--langdef=tengo",
+        "--map-tengo=+.tengo",
+        "--kinddef-tengo=f,function,function",
+        "--regex-tengo=/^\\s*(.*)(:| :=| =) ?func.*/\\1/f/",
+        "--kinddef-tengo=c,constant,constant",
+        "--regex-tengo=/^\\s*(.*) := (\"|\\{).*/\\1/c/",
+        "-R", ...tengoFiles
+      ],
+      {
+        env: process.env,
+        stdio: 'inherit'
+      }
+    );
+
+    if (result.error?.message.includes('ENOENT')) {
+      console.log(`
+pl-tengo can create tags for tengo autocompletion,
+but the program should be installed
+with "brew install universal-ctags" on OSX
+or "sudo apt install universal-ctags" on Ubuntu.
+
+For vscode, you should also install ctags extension:
+https://marketplace.visualstudio.com/items?itemName=jaydenlin.ctags-support`)
+
+      return
+    }
+
+    checkRunError(result, 'failed to generate ctags');
+
+    logger.info('Generation of tags is done.')
+  })
+}
+
+function checkRunError(result: SpawnSyncReturns<Buffer>, message?: string) {
+  if (result.error) {
+    throw result.error;
+  }
+
+  const msg = message ?? 'failed to run command';
+
+  if (result.status !== 0) {
+    throw new Error(`${msg}, process exited with code '${result.status}'`);
   }
 }
