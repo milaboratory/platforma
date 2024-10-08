e7406a9304a2ce9daa847c52d8ea4a097b802d73
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2024-10-09 00:24:46 +0300

    additional test for pl url parsing

--- lib/node/pl-client/src/core/config.test.ts
+++ lib/node/pl-client/src/core/config.test.ts
@@ -1,4 +1,4 @@
-import { plAddressToConfig } from './config';
+import { DEFAULT_TX_TIMEOUT, plAddressToConfig } from './config';
 
 test('config form url no auth', () => {
   const conf = plAddressToConfig('http://127.0.0.1:6345');
@@ -16,4 +16,12 @@ test('config form url with auth and special symbols', () => {
   const conf = plAddressToConfig('http://user1:password232$@127.0.0.1:6345');
   expect(conf.user).toEqual('user1');
   expect(conf.password).toEqual('password232$');
+  expect(conf.defaultTransactionTimeout).toEqual(DEFAULT_TX_TIMEOUT);
+});
+
+test('config form url with auth and special symbols and request timeout', () => {
+  const conf = plAddressToConfig('http://user1:password232$@127.0.0.1:6345/?tx-timeout=11341');
+  expect(conf.user).toEqual('user1');
+  expect(conf.password).toEqual('password232$');
+  expect(conf.defaultTransactionTimeout).toEqual(11341);
 });
