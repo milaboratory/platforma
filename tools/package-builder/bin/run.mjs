7e400405a69438451a2cafdbe281e5556e07a708
Korenevskiy Denis <denkoren@gmail.com>
2024-10-24 14:32:06 +0300

    fix: make package-builder to always print stacktrace of unhandled errors

---
+++ tools/package-builder/bin/run.mjs
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+import {run, handle, flush} from '@oclif/core'
+
+// eslint-disable-next-line unicorn/prefer-top-level-await
+run(process.argv.slice(2), import.meta.url)
+  .catch(async (error) => {
+    console.log(error) // write full stacktrace of any unhandled error reached this line
+    return handle(error)
+  })
+  .finally(async () => flush())
