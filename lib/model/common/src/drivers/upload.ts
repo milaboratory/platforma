c6a669e03707ff5900535234d0dfc49c22e9f54e
Gleb Zakharov <snyssfx@posteo.net>
2024-10-28 18:39:18 +0100

    sdk + drivers: upload: rename API to make it more client-friendly

--- lib/model/common/src/drivers/upload.ts
+++ lib/model/common/src/drivers/upload.ts
@@ -1,3 +1,35 @@
+export interface ImportState {
+  readonly importType: 'Index' | 'Upload';
+  readonly done: boolean;
+
+  /** Status of indexing/uploading that was got from platforma gRPC.
+   * A float from 0 to 1 and is equal to bytesProcessed / bytesTotal.
+   * It could be undefined when Platforma Backend
+   * has not sent the status yet or
+   * when the process was deduplicated and is already done. */
+  readonly progress?: ImportStateProgress;
+
+  /** True if importType is "Upload",
+   * and the file was got from this computer
+   * (i.e. a signature of the resource matched with
+   * the client's signature). */
+  readonly isLocalFileUploadStarted: boolean;
+
+  /** Exists when an upload failed and was restarted,
+   * but the error was recoverable.
+   * If the error was non-recoverable,
+   * the driver's computable will throw an error instead. */
+  readonly lastUploadMessage?: string;
+}
+
+export interface ImportStateProgress {
+  /** A float from 0 to 1 and is equal to bytesProcessed / bytesTotal. */
+  readonly progress: number;
+  readonly bytesProcessed: number;
+  readonly bytesTotal: number;
+}
+
+/** @deprecated, use ImportState instead. */
 export interface ImportProgress {
   done: boolean;
   /** Status of indexing/uploading got from platforma gRPC. */
@@ -13,10 +45,13 @@ export interface ImportProgress {
   lastError?: string;
 }
 
-/** Almost direct mapping from proto struct got from gRPC. */
+/** @deprecated, use ImportState instead.
+ * Almost direct mapping from proto struct got from gRPC. */
 export interface ImportStatus {
   /** A float from 0 to 1 and is equal to bytesProcessed / bytesTotal. */
   readonly progress: number;
   readonly bytesProcessed?: number;
   readonly bytesTotal?: number;
 }
+
+
