bc2bcf569e93e454984d8c367c983b858bf84cbd
nikolaygiman <gimanhead@gmail.com>
2024-11-06 20:47:34 +0100

    fix: added password type for PlTextField

---
+++ etc/block-components/ui-examples/ui/src/pages/PlTextFieldPage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { PlBlockPage, PlTextField } from '@platforma-sdk/ui-vue';
+import { ref } from 'vue';
+const model = ref('Some text');
+</script>
+<template>
+  <PlBlockPage style="max-width: 100%">
+    <div class="d-flex flex-column gap-16" style="width: 300px">
+      <div>Text</div>
+      <PlTextField v-model="model" placeholder="Text" />
+      <div>Password</div>
+      <PlTextField v-model="model" placeholder="Password" type="password" />
+    </div>
+  </PlBlockPage>
+</template>
