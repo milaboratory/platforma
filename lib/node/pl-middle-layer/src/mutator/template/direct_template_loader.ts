dd1dd54db704ace89c9e33fc9856847664e6ffed
Korenevskiy Denis <denkoren@gmail.com>
2024-12-19 13:58:31 +0300

    stable hashing to deduplicate even more

--- lib/node/pl-middle-layer/src/mutator/template/direct_template_loader.ts
+++ lib/node/pl-middle-layer/src/mutator/template/direct_template_loader.ts
@@ -70,19 +70,24 @@ const TemplateRenderer: Renderer<TemplateData> = {
       .update(resource.version)
       .update(resource.src);
 
-    for (const [libId, lib] of Object.entries(resource.libs ?? {})) {
+    const srt = <T>(entries: [string, T][]) : [string, T][] => {
+      entries.sort((a, b) => a[0] === b[0] ? 0 : a[0] < b[0] ? -1 : 1);
+      return entries;
+    };
+
+    for (const [libId, lib] of srt(Object.entries(resource.libs ?? {}))) {
       hash.update('lib:' + libId);
       LibRenderer.hash(lib, hash);
     }
-    for (const [swId, sw] of Object.entries(resource.software ?? {})) {
+    for (const [swId, sw] of srt(Object.entries(resource.software ?? {}))) {
       hash.update('soft:' + swId);
       SoftwareInfoRenderer.hash(sw, hash);
     }
-    for (const [swId, sw] of Object.entries(resource.assets ?? {})) {
+    for (const [swId, sw] of srt(Object.entries(resource.assets ?? {}))) {
       hash.update('asset:' + swId);
       SoftwareInfoRenderer.hash(sw, hash);
     }
-    for (const [tplId, tpl] of Object.entries(resource.templates ?? {})) {
+    for (const [tplId, tpl] of srt(Object.entries(resource.templates ?? {}))) {
       hash.update('tpl:' + tplId);
       this.hash(tpl, hash);
     }
