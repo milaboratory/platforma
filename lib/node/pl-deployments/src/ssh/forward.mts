49693965e727d464169e851053fd496c8b81c36b
nikolaygiman <gimanhead@gmail.com>
2025-01-03 12:25:04 +0100

    wip: port forward

---
+++ lib/node/pl-deployments/src/ssh/forward.mts
@@ -0,0 +1,23 @@
+import { getPrivateKey, SSH_TEST_HOST, SSH_TEST_USER } from './connections.secret';
+import { forwardMultiplePorts } from './ssh';
+
+const config = {
+  host: SSH_TEST_HOST,
+  username: SSH_TEST_USER,
+  privateKey: getPrivateKey(),
+};
+forwardMultiplePorts(config, { remotePort: 1415, localPort: 1415 });
+
+forwardMultiplePorts(config, { remotePort: 42097, localPort: 42097 });
+
+// forwardMultiplePorts(config, { remotePort: 37659, localPort: 37659 });
+
+// forwardMultiplePorts(config, { remotePort: 39841, localPort: 39841 });
+
+// forwardMultiplePorts(config, { remotePort: 9000, localPort: 9000 });
+
+// forwardMultiplePorts({
+//   host: SSH_TEST_HOST,
+//   username: SSH_TEST_USER,
+//   privateKey: getPrivateKey(),
+// }, { remotePort: 9001, localPort: 9001 });
--- lib/node/pl-deployments/src/ssh/ssh.ts
+++ lib/node/pl-deployments/src/ssh/ssh.ts
@@ -1,7 +1,9 @@
-import { ConnectConfig, Client, Connection, ClientChannel } from "ssh2";
+import type { ConnectConfig, ClientChannel } from 'ssh2';
+import { Client, Connection } from 'ssh2';
+import net from 'net';
 
 /** Promisified exec on ssh connection. */
-export function sshExec(client: Client, command: string): Promise<{stdout: string, stderr: string}> {
+export function sshExec(client: Client, command: string): Promise<{ stdout: string; stderr: string }> {
   return new Promise((resolve, reject) => {
     client.exec(command, (err: any, stream: ClientChannel) => {
       if (err) return reject(err);
@@ -24,7 +26,9 @@ export function sshExec(client: Client, command: string): Promise<{stdout: strin
   });
 }
 
-/** Promisified connect function for ssh Client. */
+/**
+ * Promisified connect function for ssh Client.
+ */
 export function sshConnect(client: Client, config: ConnectConfig): Promise<undefined> {
   return new Promise((resolve, reject) => {
     client.on('ready', () => {
@@ -32,7 +36,130 @@ export function sshConnect(client: Client, config: ConnectConfig): Promise<undef
     }).on('error', (err: any) => {
       reject(err);
     }).on('timeout', () => {
-      reject(new Error(`timeout was occurred while waiting for SSH connection.`))
+      reject(new Error(`timeout was occurred while waiting for SSH connection.`));
     }).connect(config);
   });
 }
+/**
+ * Get auth types for host
+ * @param host - remote host
+ * @returns string[] //['publickey', 'password']
+ */
+export async function sshGetAuthTypes(host: string) {
+  return new Promise((resolve) => {
+    let stdout = '';
+    sshConnect(new Client(), {
+      host,
+      username: new Date().getTime().toString(),
+      debug: (err: string) => {
+        stdout += `${err} \n`;
+      },
+    })
+      .catch((err) => err)
+      .finally(() => {
+        const types = extractAuthMethods(stdout);
+        resolve(types.length === 0 ? ['publickey', 'password'] : types);
+      });
+  });
+}
+
+function extractAuthMethods(log: string): string[] {
+  const newFormatMatch = log.match(/Inbound: Received USERAUTH_FAILURE \((.+)\)/);
+  if (newFormatMatch && newFormatMatch[1]) {
+    return newFormatMatch[1].split(',').map((method) => method.trim());
+  }
+  return [];
+}
+
+export function forwardMultiplePorts(config: ConnectConfig, ports: { remotePort: number; localPort: number; localHost?: string }) {
+  const conn = new Client();
+  conn.on('ready', () => {
+    console.log(`[SSH] Подключено к ${config.host}. Открываем локальный сервер на порт ${ports.localPort}.`);
+    console.log(`[SSH] Трафик будет перенаправлен на :${ports.remotePort}`);
+    // Создаём локальный TCP-сервер, слушающий localhost:localPort
+    net.createServer((localSocket) => {
+      console.log('localSocket');
+      // При каждом новом входящем соединении открываем канал forwardOut
+      conn.forwardOut(
+        // Эмулируем, что "источник" — это 127.0.0.1:0
+        '127.0.0.1',
+        0,
+        'localhost', // remoteHost,
+        ports.remotePort,
+        (err, stream) => {
+          if (err) {
+            console.error('Ошибка при открытии SSH-канала:', err.message);
+            localSocket.end();
+            return;
+          }
+          // Перенаправляем все данные socket <-> stream
+          localSocket.pipe(stream);
+          stream.pipe(localSocket);
+        },
+      );
+    }).listen(ports.localPort, '127.0.0.1', () => {
+      console.log(`[+] Порт ${ports.localPort} доступен локально → :${ports.remotePort}`);
+      console.log('Откройте свой gRPC (или другой) клиент на localhost:' + ports.localPort);
+    });
+  });
+
+  conn.on('error', (err) => {
+    console.error('[SSH] Ошибка при SSH-подключении:', err.message);
+  });
+
+  conn.on('close', () => {
+    console.log('[SSH] Подключение закрыто');
+  });
+
+  // Инициируем SSH-подключение
+  conn.connect({ ...config, debug(err: string) {
+    console.log(err);
+  } });
+}
+// export function forwardMultiplePorts(config: ConnectConfig, ports: { remotePort: number; localPort: number; localHost?: string }) {
+//   const conn = new Client();
+
+//   conn.on('ready', () => {
+//     console.log(`[SSH] Подключено к ${config.host}. Открываем локальный сервер на порт ${ports.localPort}.`);
+//     console.log(`[SSH] Трафик будет перенаправлен на :${ports.remotePort}`);
+
+//     // Создаём локальный TCP-сервер, слушающий localhost:localPort
+//     net.createServer((localSocket) => {
+//       localSocket.setKeepAlive(true, 60000);
+//       // При каждом новом входящем соединении открываем канал forwardOut
+//       conn.forwardOut(
+//         // Эмулируем, что "источник" — это 127.0.0.1:0
+//         '127.0.0.1',
+//         0,
+//         'localhost', // remoteHost,
+//         ports.remotePort,
+//         (err, stream) => {
+//           if (err) {
+//             console.error('Ошибка при открытии SSH-канала:', err.message);
+//             localSocket.end();
+//             return;
+//           }
+//           // Перенаправляем все данные socket <-> stream
+//           localSocket.pipe(stream);
+//           stream.pipe(localSocket);
+//         },
+//       );
+//     }).listen(ports.localPort, '127.0.0.1', () => {
+//       console.log(`[+] Порт ${ports.localPort} доступен локально → :${ports.remotePort}`);
+//       console.log('Откройте свой gRPC (или другой) клиент на localhost:' + ports.localPort);
+//     });
+//   });
+
+//   conn.on('error', (err) => {
+//     console.error('[SSH] Ошибка при SSH-подключении:', err.message);
+//   });
+
+//   conn.on('close', () => {
+//     console.log('[SSH] Подключение закрыто');
+//   });
+
+//   // Инициируем SSH-подключение
+//   conn.connect({ ...config, debug(err: string) {
+//     console.log(err);
+//   } });
+// }
