1afdf0f68bda9e8b36eaabe437423e15ec23225a
Gleb Zakharov <snyssfx@posteo.net>
2025-01-22 11:09:18 +0100

    drivers: blob to url: fix tests

--- lib/node/pl-drivers/src/drivers/download_blob_url/url.test.ts
+++ lib/node/pl-drivers/src/drivers/download_blob_url/url.test.ts
@@ -7,7 +7,7 @@ import { FolderURL } from '@milaboratories/pl-model-common';
 
 describe('isFolderURL', () => {
   test('should return true for a valid URL', () => {
-    const folderUrl = 'plblob+folder://5976f110166cc5d8803c41181fbb708470b69075db19ad98d8971df350732028.blob-31330/path/to/folder';
+    const folderUrl = 'plblob+folder://5976f110166cc5d8803c41181fbb708470b69075db19ad98d8971df350732028.31330_tgz.blob/path/to/folder';
     expect(isFolderURL(folderUrl)).toBe(true);
   });
 
@@ -30,10 +30,10 @@ describe('getPathForFolderURL', () => {
   };
 
   test('should be ok', () => {
-    const folderURL = 'plblob+folder://signature.blob-subfolder/path/to/resource.html';
+    const folderURL = 'plblob+folder://signature.subfolder_tgz.blob/path/to/resource.html';
 
     const result = getPathForFolderURL(signer, folderURL, '/test/dir');
 
-    expect(result).toBe(path.join('/test/dir/subfolder/path/to/resource.html'));
+    expect(result).toBe(path.join('/test/dir/subfolder_tgz/path/to/resource.html'));
   });
 });
