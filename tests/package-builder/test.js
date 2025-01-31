7e012dd6789fe65b727800a3741522d9845ff631
Gleb Zakharov <snyssfx@posteo.net>
2025-01-31 12:30:29 +0100

    fix package-builder tests

--- tests/package-builder/test.js
+++ tests/package-builder/test.js
@@ -26,8 +26,8 @@ directories.forEach((directory) => {
   try {
     process.stdout.write(`  ${formatWidth(directory, width)}: `);
 
-    execSync('pnpm install --frozen-lockfile', { cwd: path.join(scriptDir, directory), stdio: 'pipe' })
-    execSync('pnpm run build', { cwd: path.join(scriptDir, directory), stdio: 'pipe' });
+    execSync('pnpm install', { cwd: path.join(scriptDir, directory) })
+    execSync('pnpm run build', { cwd: path.join(scriptDir, directory) });
 
     process.stdout.write(`OK\n`);
   } catch (error) {
