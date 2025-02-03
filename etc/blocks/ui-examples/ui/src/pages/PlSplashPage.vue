b2d6bf113ccf7244d773cce98a48ad46a0019dbb
andrewkhizhnyak <khizhnyak.andrew@gmail.com>
2025-02-03 18:28:06 +0100

    MILAB-478: add PlSplash page to examples

---
+++ etc/blocks/ui-examples/ui/src/pages/PlSplashPage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { PlBlockPage, PlSplash } from '@platforma-sdk/ui-vue';
+</script>
+
+<template>
+  <PlBlockPage style="height: 100vh; display: flex; align-items: center; justify-content: center">
+    <PlSplash size="48" text="Loading" />
+  </PlBlockPage>
+</template>
