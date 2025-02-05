c00adc6ff179e203f156db72148191e4c018a643
andrewkhizhnyak <khizhnyak.andrew@gmail.com>
2025-02-05 15:04:34 +0100

    MILAB-478: add Page loading to PlSplash examples

--- etc/blocks/ui-examples/ui/src/pages/PlSplashPage.vue
+++ etc/blocks/ui-examples/ui/src/pages/PlSplashPage.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { faker } from '@faker-js/faker';
 import { PlBlockPage, PlBtnPrimary, PlCheckbox, PlContainer, PlRow, PlSplash, PlTextField } from '@platforma-sdk/ui-vue';
-import { reactive } from 'vue';
+import { reactive, ref } from 'vue';
 
 const lorem1 = faker.lorem.paragraph(5);
 const lorem2 = faker.lorem.paragraph(5);
@@ -13,15 +13,25 @@ const form = reactive({
   agreed: false,
 });
 
+const showPage = ref(true);
+const timeoutId = ref(0);
+
+const handleReloadPage = () => {
+  clearTimeout(timeoutId.value);
+  showPage.value = false;
+  timeoutId.value = setTimeout(() => {
+    showPage.value = true;
+  }, 3000);
+};
 </script>
 
 <template>
   <PlBlockPage>
     <template #title>PlSplash Component</template>
     <template #append>
-      <PlBtnPrimary>Reload page</PlBtnPrimary>
+      <PlBtnPrimary @click="handleReloadPage">Reload page</PlBtnPrimary>
     </template>
-    <PlRow>
+    <PlRow v-if="showPage">
       <PlContainer width="50%">
         <h3>Form</h3>
         <PlContainer width="400px">
@@ -39,5 +49,6 @@ const form = reactive({
         <p>{{ lorem3 }}</p>
       </PlContainer>
     </PlRow>
+    <PlSplash v-else text="Page loading" />
   </PlBlockPage>
 </template>
