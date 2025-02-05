1192a7c4ef67aba3a291407112f117808545bd5f
gramkin <bakluzhino@gmail.com>
2025-02-05 13:18:51 +0100

    update ui-examples/PlSplashPage

--- etc/blocks/ui-examples/ui/src/pages/PlSplashPage.vue
+++ etc/blocks/ui-examples/ui/src/pages/PlSplashPage.vue
@@ -1,7 +1,43 @@
 <script setup lang="ts">
-import { PlSplash } from '@platforma-sdk/ui-vue';
+import { faker } from '@faker-js/faker';
+import { PlBlockPage, PlBtnPrimary, PlCheckbox, PlContainer, PlRow, PlSplash, PlTextField } from '@platforma-sdk/ui-vue';
+import { reactive } from 'vue';
+
+const lorem1 = faker.lorem.paragraph(5);
+const lorem2 = faker.lorem.paragraph(5);
+const lorem3 = faker.lorem.paragraph(5);
+
+const form = reactive({
+  name: '',
+  surname: '',
+  agreed: false,
+});
+
 </script>
 
 <template>
-  <PlSplash size="48" text="Loading" />
+  <PlBlockPage>
+    <template #title>PlSplash Component</template>
+    <template #append>
+      <PlBtnPrimary>Reload page</PlBtnPrimary>
+    </template>
+    <PlRow>
+      <PlContainer width="50%">
+        <h3>Form</h3>
+        <PlContainer width="400px">
+          <PlTextField v-model="form.name" label="Name" />
+          <PlTextField v-model="form.surname" label="Surname" />
+          <PlCheckbox v-model="form.agreed">I agreed to...</PlCheckbox>
+          <PlBtnPrimary>Submit form</PlBtnPrimary>
+        </PlContainer>
+      </PlContainer>
+      <PlContainer width="50%">
+        <h3>Title</h3>
+        <p>{{ lorem1 }}</p>
+        <p>{{ lorem2 }}</p>
+        <PlBtnPrimary>Reload the second paragraph</PlBtnPrimary>
+        <p>{{ lorem3 }}</p>
+      </PlContainer>
+    </PlRow>
+  </PlBlockPage>
 </template>
