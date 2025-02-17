1789f1ecf72a08fe415c22001a21dddd4d61da07
Vadim Piven <vadim.piven@milaboratories.com>
2025-02-17 13:26:14 +0400

    PFrameV3 interface export

--- lib/model/middle-layer/src/pframe/internal_api/pframe.ts
+++ lib/model/middle-layer/src/pframe/internal_api/pframe.ts
@@ -1,5 +1,5 @@
 import type { PFrameFactoryAPI } from './api_factory';
-import type { PFrameReadAPIV2 } from './api_read';
+import type { PFrameReadAPIV2, PFrameReadAPIV3 } from './api_read';
 
 export type Logger = (
   level: 'info' | 'warn' | 'error',
@@ -7,3 +7,5 @@ export type Logger = (
 ) => void;
 
 export interface PFrameV2 extends PFrameFactoryAPI, PFrameReadAPIV2 {}
+
+export interface PFrameV3 extends PFrameFactoryAPI, PFrameReadAPIV3 {}
