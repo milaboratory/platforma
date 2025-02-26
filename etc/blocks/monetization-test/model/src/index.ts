53c1052dc1bc137a320dad151174164e83ff21d1
Gleb Zakharov <snyssfx@posteo.net>
2025-02-26 18:06:04 +0100

    mnz: more specified test block

--- etc/blocks/monetization-test/model/src/index.ts
+++ etc/blocks/monetization-test/model/src/index.ts
@@ -1,21 +1,33 @@
-import { BlockModel, ImportFileHandle, InferOutputsType } from '@platforma-sdk/model';
+import { BlockModel, ImportFileHandle, ImportProgress, InferOutputsType } from '@platforma-sdk/model';
+
+export type Handle = {
+  handle: ImportFileHandle | undefined;
+  fileName: string;
+  argName: string;
+  options: string[];
+}
 
 export type BlockArgs = {
-  commandToMonetize: string[];
   productKey: string;
-  // inputHandles: (ImportFileHandle | undefined)[];
+  inputHandles: Handle[];
 };
 
 export const model = BlockModel.create()
-  .withArgs({
-    commandToMonetize: ["/usr/bin/env", "bash", "-c", "echo -n $PLATFORMA_MNZ_JWT"],
+  .withArgs<BlockArgs>({
     // a fake product key so our mnz client response with a fake response without changing prod db.
     productKey: "MIFAKEMIFAKEMIFAKE",
+    inputHandles: [],
   })
 
   .output('info', (ctx) => ctx.prerun?.resolve('info')?.getDataAsJson<unknown>())
 
-  .output('stdout', (ctx) => ctx.outputs?.resolve('stdout')?.getDataAsString())
+  .output('token', (ctx) => ctx.outputs?.resolve('token')?.getDataAsString())
+
+  .output('progresses', (ctx) => {
+    const m = ctx.outputs?.resolve('progresses');
+    const progresses = m?.mapFields((name, val) => [name, val?.getImportProgress()])
+    return Object.fromEntries(progresses ?? []);
+  })
 
   .sections((_) => [{ type: 'link', href: '/', label: 'Main' }])
 
--- etc/blocks/monetization-test/ui/src/pages/MainPage.vue
+++ etc/blocks/monetization-test/ui/src/pages/MainPage.vue
@@ -1,26 +1,99 @@
 <script setup lang="ts">
-import { PlAlert, PlBlockPage, PlTextField } from '@platforma-sdk/ui-vue';
+import type { ImportFileHandle } from '@platforma-sdk/model';
+import type { ImportedFiles, ListOption } from '@platforma-sdk/ui-vue';
+
+import { PlAlert, PlBlockPage, PlContainer, PlRow, PlTextField, PlBtnPrimary, PlFileDialog, PlFileInput, PlDropdownMulti } from '@platforma-sdk/ui-vue';
 import { useApp } from '../app';
-import { computed, reactive } from 'vue';
+import { computed, reactive, ref } from 'vue';
 
 const app = useApp();
 
-const commandToMonetize = computed({
-  get: () => app.model.args.commandToMonetize.join(','),
-  set: (val: string) => {
-    console.log(val.split(","))
-    app.model.args.commandToMonetize = val.split(',')
+const dropdownOptions: ListOption<string>[] = [
+  {
+    text: 'sha256',
+    value: 'sha256'
+  },
+  {
+    text: 'lines (only in .zip files)',
+    value: 'lines',
+  },
+  {
+    text: 'size',
+    value: 'size'
   }
+];
+
+const files = reactive<{
+  isMultiDialogFileOpen: boolean;
+}>({
+  isMultiDialogFileOpen: false,
 })
+
+const updateHandle = (v: ImportFileHandle | undefined, i: number) => {
+  if (v) {
+    app.model.args.inputHandles[i].handle = v;
+  } else {
+    app.model.args.inputHandles.splice(i, 1);
+  }
+};
+
+const onImport = (imported: ImportedFiles) => {
+  app.model.args.inputHandles = imported.files.map((h, i) => ({
+    handle: h,
+    fileName: `test${i}.txt`,
+    argName: `arg_${i}`,
+    options: ['size', 'sha256']
+  }));
+};
 
 const parsedToken = computed({
   get: () => {
-    const splitted = app.model.outputs.stdout?.split('.') ?? [];
+    const splitted = app.model.outputs.token?.split('.') ?? [];
     const data = splitted[1];
     return JSON.parse(atob(data));
   },
   set: () => {}
 })
+
+const verificationResult = ref('');
+
+const publicKey = 'MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAECGShTw8Plag1uMuCg9OMYVHCF+wzjvXKr3cihyO77jEe9CrF6RP9tfnCd2XjM7XqQ0QH3i41rz5ohCB9fDDBbQ==';
+
+async function verify(token: string) {
+  const cryptoPublicKey = await crypto.subtle.importKey(
+    'spki',
+    Uint8Array.from(atob(publicKey), c => c.charCodeAt(0)).buffer,
+    { name: 'ECDSA', namedCurve: 'P-256' },
+    true,
+    ['verify']
+  );
+
+  const [base64Header, base64Payload, signature] = token.split('.');
+  if (!base64Header || typeof base64Payload !== 'string' || typeof signature !== 'string')
+    throw new Error('Invalid token body');
+
+  const signatureBinary = Uint8Array.from(
+    atob(signature.replace(/-/g, '+').replace(/_/g, '/')),
+    c => c.charCodeAt(0)
+  );
+
+  try {
+    const result = await crypto.subtle.verify(
+      { name: 'ECDSA', hash: { name: 'SHA-256' } },
+      cryptoPublicKey,
+      signatureBinary,
+      new TextEncoder().encode(`${base64Header}.${base64Payload}`)
+    );
+
+    if (result)
+      verificationResult.value = "Signature is correct";
+    else
+      verificationResult.value = "Signature is incorrect";
+  } catch (e: unknown) {
+    verificationResult.value = "Verification failed";
+  }
+}
+
 
 </script>
 
@@ -30,25 +103,40 @@ const parsedToken = computed({
     <PlTextField v-model="app.model.args.productKey"
       label="Enter product key (keep MIFAKEMIFAKEMIFAKE for fake product)" clearable />
 
-    <PlTextField v-model="commandToMonetize"
-      label="Enter command to monetize (separate args by comma and use $PLATFORMA_MNZ_JWT for the token)" clearable />
+    <PlContainer width="400px">
+      <PlBtnPrimary @click="files.isMultiDialogFileOpen = true">
+        Open multiple files to monetize
+      </PlBtnPrimary>
+    </PlContainer>
+    <template v-for="({ handle }, i) of app.model.args.inputHandles" :key="i">
+      <PlRow>
+        <PlTextField v-model="app.model.args.inputHandles[i].fileName" label="Type file name" />
+        <PlTextField v-model="app.model.args.inputHandles[i].argName" label="Type argument name" />
+        <PlFileInput :model-value="handle"
+          @update:model-value="(v: ImportFileHandle | undefined) => updateHandle(v, i)" />
+        <PlDropdownMulti label="Metrics to monetize" v-model="app.model.args.inputHandles[i].options"
+          :options="dropdownOptions" />
+      </PlRow>
+    </template>
+    <PlFileDialog v-model="files.isMultiDialogFileOpen" multi @import:files="onImport" />
 
-    <!-- <PlFileInput v-model="app.model.args.inputHandle" label="Select file to import" /> -->
-
+    <PlContainer />
     <PlAlert label="pre-run info" v-if="app.model.outputs.info">
       <pre> {{ JSON.stringify(app.model.outputs.info, undefined, 2) }} </pre>
     </PlAlert>
 
-    <PlAlert label="stdout of the command" v-if="app.model.outputs.stdout"> {{ app.model.outputs.stdout }}
+    <PlAlert label="token" v-if="app.model.outputs.token"> {{ app.model.outputs.token }}
     </PlAlert>
 
-    <PlAlert label="trying to parse a token" v-if="app.model.outputs.stdout">
+    <PlBtnPrimary v-if="app.model.outputs.token" @click="verify(app.model.outputs.token)">
+      Verify</PlBtnPrimary>
+
+    <PlAlert label="token verification" v-if="verificationResult"> {{ verificationResult }}
+    </PlAlert>
+
+    <PlAlert label="trying to parse a token" v-if="app.model.outputs.token">
       <pre> {{ JSON.stringify(parsedToken, null, 2) }} </pre>
     </PlAlert>
-
-
-    <!-- <PlAlert type="success" label="resulted token" v-if="app.model.outputs.token"> {{ app.model.outputs.token }}
-    </PlAlert> -->
 
   </PlBlockPage>
 </template>
