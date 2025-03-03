63445df63d37be7b69b62cf3447daf42db6c3ca4
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2025-03-03 11:40:58 +0200

    test: Add comprehensive tests for pframes.remove-nulls template

--- tests/workflow-tengo/src/pframes/test/remove-nulls.test.ts
+++ tests/workflow-tengo/src/pframes/test/remove-nulls.test.ts
@@ -1,4 +1,5 @@
-import { field, Pl, resourceType } from '@milaboratories/pl-middle-layer';
+import type { ResourceId } from '@milaboratories/pl-middle-layer';
+import { field, Pl, resourceType, toGlobalResourceId } from '@milaboratories/pl-middle-layer';
 import { awaitStableState, tplTest } from '@platforma-sdk/test';
 import { Templates as SdkTemplates } from '@platforma-sdk/workflow-tengo';
 
@@ -27,6 +28,129 @@ tplTest.for([
         return { map: inputMap };
       },
     );
+
+    const resultMapLength = await awaitStableState(result.computeOutput('result', (a) => {
+      const fields = a?.listInputFields();
+      return Object.fromEntries(fields?.map((f) => [f, a?.traverse(f)?.getDataAsJson()]) ?? []);
+    }));
+    expect(resultMapLength).toStrictEqual({
+      '[1]': { a: 1 },
+      '[3]': { a: 3 },
+    });
+  },
+);
+
+tplTest.for([
+  { isEph: true, name: 'ephemeral mode' },
+  { isEph: false, name: 'pure mode' },
+])(
+  'should correctly execute pframes.remove-nulls with all nulls($name)',
+  { timeout: 10000 },
+  async ({ isEph }, { helper, expect }) => {
+    const result = await helper.renderTemplate(
+      isEph,
+      SdkTemplates['pframes.remove-nulls'],
+      ['result'],
+      (tx) => {
+        const inputMap = tx.createStruct(
+          resourceType('PColumnData/ResourceMap', '1'),
+          JSON.stringify({
+            keyLength: 1,
+          }),
+        );
+        tx.createField(field(inputMap, '[1]'), 'Input', tx.createValue(Pl.RNull, ''));
+        tx.createField(field(inputMap, '[2]'), 'Input', tx.createValue(Pl.RNull, ''));
+        tx.createField(field(inputMap, '[3]'), 'Input', tx.createValue(Pl.RNull, ''));
+        tx.lockInputs(inputMap);
+        return { map: inputMap };
+      },
+    );
+
+    const resultMapLength = await awaitStableState(result.computeOutput('result', (a) => {
+      const fields = a?.listInputFields();
+      return Object.fromEntries(fields?.map((f) => [f, a?.traverse(f)?.getDataAsJson()]) ?? []);
+    }));
+    expect(resultMapLength).toStrictEqual({});
+  },
+);
+
+tplTest.for([
+  { isEph: true, name: 'ephemeral mode' },
+  { isEph: false, name: 'pure mode' },
+])(
+  'should correctly execute pframes.remove-nulls delayed 1 ($name)',
+  { timeout: 10000 },
+  async ({ isEph }, { helper, pl, expect }) => {
+    let theMap: ResourceId | undefined = undefined;
+    const result = await helper.renderTemplate(
+      isEph,
+      SdkTemplates['pframes.remove-nulls'],
+      ['result'],
+      async (tx) => {
+        const inputMap = tx.createStruct(
+          resourceType('PColumnData/ResourceMap', '1'),
+          JSON.stringify({
+            keyLength: 1,
+          }),
+        );
+        theMap = await toGlobalResourceId(inputMap);
+        tx.createField(field(inputMap, '[1]'), 'Input', tx.createValue(Pl.JsonObject, JSON.stringify({ a: 1 })));
+        tx.createField(field(inputMap, '[2]'), 'Input', tx.createValue(Pl.RNull, ''));
+        return { map: inputMap };
+      },
+    );
+
+    pl.withWriteTx('setKeys', async (tx) => {
+      tx.createField(field(theMap!, '[3]'), 'Input', tx.createValue(Pl.JsonObject, JSON.stringify({ a: 3 })));
+      tx.createField(field(theMap!, '[4]'), 'Input', tx.createValue(Pl.RNull, ''));
+      tx.lockInputs(theMap!);
+      await tx.commit();
+    });
+
+    const resultMapLength = await awaitStableState(result.computeOutput('result', (a) => {
+      const fields = a?.listInputFields();
+      return Object.fromEntries(fields?.map((f) => [f, a?.traverse(f)?.getDataAsJson()]) ?? []);
+    }));
+    expect(resultMapLength).toStrictEqual({
+      '[1]': { a: 1 },
+      '[3]': { a: 3 },
+    });
+  },
+);
+
+tplTest.for([
+  { isEph: true, name: 'ephemeral mode' },
+  { isEph: false, name: 'pure mode' },
+])(
+  'should correctly execute pframes.remove-nulls delayed 2 ($name)',
+  { timeout: 10000 },
+  async ({ isEph }, { helper, pl, expect }) => {
+    let theMap: ResourceId | undefined = undefined;
+    const result = await helper.renderTemplate(
+      isEph,
+      SdkTemplates['pframes.remove-nulls'],
+      ['result'],
+      async (tx) => {
+        const inputMap = tx.createStruct(
+          resourceType('PColumnData/ResourceMap', '1'),
+          JSON.stringify({
+            keyLength: 1,
+          }),
+        );
+        theMap = await toGlobalResourceId(inputMap);
+        tx.createField(field(inputMap, '[1]'), 'Input', tx.createValue(Pl.JsonObject, JSON.stringify({ a: 1 })));
+        tx.createField(field(inputMap, '[2]'), 'Input', tx.createValue(Pl.RNull, ''));
+        tx.createField(field(inputMap, '[3]'), 'Input');
+        tx.createField(field(inputMap, '[4]'), 'Input', tx.createValue(Pl.RNull, ''));
+        tx.lockInputs(inputMap);
+        return { map: inputMap };
+      },
+    );
+
+    pl.withWriteTx('setKeys', async (tx) => {
+      tx.setField(field(theMap!, '[3]'), tx.createValue(Pl.JsonObject, JSON.stringify({ a: 3 })));
+      await tx.commit();
+    });
 
     const resultMapLength = await awaitStableState(result.computeOutput('result', (a) => {
       const fields = a?.listInputFields();
