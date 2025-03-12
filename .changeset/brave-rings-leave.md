21465ea625f46424f534cd4bc68cece03f2b8723
Gleb Zakharov <snyssfx@posteo.net>
2025-03-12 13:18:39 +0100

    mnz: pre-run fixes

---
+++ .changeset/brave-rings-leave.md
@@ -0,0 +1,8 @@
+---
+'@milaboratories/milaboratories.monetization-test.workflow': patch
+'@milaboratories/milaboratories.monetization-test.model': patch
+'@platforma-sdk/workflow-tengo-tests': patch
+'@platforma-sdk/workflow-tengo': patch
+---
+
+mnz: fixes pre-run
--- tests/workflow-tengo/src/exec/test/mnz.test.ts
+++ tests/workflow-tengo/src/exec/test/mnz.test.ts
@@ -46,18 +46,22 @@ tplTest(
 
     // faked response from mnz-client
     expect(info).toMatchObject({
-      productKey: "MIFAKEMIFAKEMIFAKE",
-      canRun: true,
-      mnz: {
-        type: "base",
-        details: {
-          spentRuns: 192,
-          runsToSpend: 1,
-          willRemainAfterRun: 7,
-          subscription: {
-            availableRuns: 200,
-            startsAt: "2025-02-25T11:50:59.000Z",
-            expiresAt: "2025-03-27T11:50:59.000Z"
+      response: {
+        result: {
+          productKey: "MIFAKEMIFAKEMIFAKE",
+          canRun: true,
+          mnz: {
+            type: "base",
+            details: {
+              spentRuns: 192,
+              runsToSpend: 1,
+              willRemainAfterRun: 7,
+              subscription: {
+                availableRuns: 200,
+                startsAt: "2025-02-25T11:50:59.000Z",
+                expiresAt: "2025-03-27T11:50:59.000Z"
+              }
+            }
           }
         }
       }
