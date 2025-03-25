097e8e2af1144e234d0c7674aa29870ce7b70cf0
Gleb Zakharov <snyssfx@posteo.net>
2025-03-25 08:41:13 +0100

    pl-errors: add error like shape

--- lib/node/pl-errors/src/index.ts
+++ lib/node/pl-errors/src/index.ts
@@ -1,1 +1,2 @@
 export * from './parsed_error';
+export * from './error_like_shape';
--- lib/node/pl-tree/src/accessors.ts
+++ lib/node/pl-tree/src/accessors.ts
@@ -23,9 +23,8 @@ import {
   ResourceTraversalOps
 } from './traversal_ops';
 import { ValueOrError } from './value_or_error';
-import { parsePlError } from '@milaboratories/pl-errors';
+import { parsePlError, ErrorLike, PlErrorReport } from '@milaboratories/pl-errors';
 import { notEmpty } from '@milaboratories/ts-helpers';
-import { ErrorLike } from '@milaboratories/computable';
 
 /** Error encountered during traversal in field or resource. */
 export class PlError extends Error {
@@ -223,13 +222,13 @@ export class PlTreeNodeAccessor {
         errorIfFieldNotSet: true;
       }
     ]
-  ): ValueOrError<PlTreeNodeAccessor, ErrorLike>;
+  ): ValueOrError<PlTreeNodeAccessor, Error>;
   public traverseOrError(
     ...steps: (FieldTraversalStep | string)[]
-  ): ValueOrError<PlTreeNodeAccessor, ErrorLike> | undefined;
+  ): ValueOrError<PlTreeNodeAccessor, Error> | undefined;
   public traverseOrError(
     ...steps: (FieldTraversalStep | string)[]
-  ): ValueOrError<PlTreeNodeAccessor, ErrorLike> | undefined {
+  ): ValueOrError<PlTreeNodeAccessor, Error> | undefined {
     return this.traverseOrErrorWithCommon({}, ...steps);
   }
 
@@ -246,7 +245,7 @@ export class PlTreeNodeAccessor {
   public traverseOrErrorWithCommon(
     commonOptions: CommonFieldTraverseOps,
     ...steps: (FieldTraversalStep | string)[]
-  ): ValueOrError<PlTreeNodeAccessor, PlErrorReport> | undefined {
+  ): ValueOrError<PlTreeNodeAccessor, Error> | undefined {
     let current: PlTreeNodeAccessor = this;
 
     for (const _step of steps) {
@@ -270,17 +269,17 @@ export class PlTreeNodeAccessor {
           ok: false,
 
           // FIXME: in next tickets we'll allow Errors to be thrown.
-          error: (parsePlError(
+          error: parsePlError(
             notEmpty(next.error.getDataAsString()),
             current.id, current.resourceType, step.field,
-          ) as unknown as string),
+          ),
         };
 
       if (next.value === undefined) {
         if (step.errorIfFieldNotSet)
           return {
             ok: false,
-            error: `field have no assigned value ${step.field} of ${resourceIdToString(current.id)}`,
+            error: new Error(`field have no assigned value ${step.field} of ${resourceIdToString(current.id)}`),
           };
         // existing but unpopulated field is unstable because it must be resolved at some point
         this.onUnstableLambda('unpopulated_field:' + step.field);
