e82d0b88e2d6934bdbc01fb650d148eea6a9f9bb
Vadim Piven <vadim.piven@milaboratories.com>
2025-04-02 22:26:26 +0200

    SlicedColumnJoinEntry definition

--- lib/model/middle-layer/src/pframe/internal_api/api_read.ts
+++ lib/model/middle-layer/src/pframe/internal_api/api_read.ts
@@ -7,7 +7,7 @@ import {
   UniqueValuesRequest,
   UniqueValuesResponse
 } from '@milaboratories/pl-model-common';
-import { CreateTableRequest } from './create_table';
+import { CreateTableRequest, CreateTableRequestV2 } from './create_table';
 import { PTableV3 } from './table';
 
 /** Read interface exposed by PFrames library */
@@ -33,6 +33,34 @@ export interface PFrameReadAPIV3 {
 
   /** Calculates data for the table and returns an object to access it */
   createTable(request: CreateTableRequest, signal?: AbortSignal): Promise<PTableV3>;
+
+  /** Calculate set of unique values for a specific axis for the filtered set of records */
+  getUniqueValues(request: UniqueValuesRequest, signal?: AbortSignal): Promise<UniqueValuesResponse>;
+}
+
+/** Read interface exposed by PFrames library */
+export interface PFrameReadAPIV4 {
+  /**
+   * Finds columns given filtering criteria on column name, annotations etc.
+   * and a set of qualified axes specs to find only columns with compatible
+   * axes spec.
+   *
+   * Only column specs are used, this method will work even for columns
+   * with no assigned data.
+   * */
+  findColumns(request: FindColumnsRequest): Promise<FindColumnsResponse>;
+
+  /** To be reviewed in the future */
+  deleteColumn(request: DeleteColumnFromColumnsRequest): Promise<DeleteColumnFromColumnsResponse>;
+
+  /** Retrieve single column spec */
+  getColumnSpec(columnId: PObjectId): Promise<PColumnSpec>;
+
+  /** Retrieve information about all columns currently added to the PFrame */
+  listColumns(): Promise<PColumnInfo[]>;
+
+  /** Calculates data for the table and returns an object to access it */
+  createTable(request: CreateTableRequestV2, signal?: AbortSignal): Promise<PTableV3>;
 
   /** Calculate set of unique values for a specific axis for the filtered set of records */
   getUniqueValues(request: UniqueValuesRequest, signal?: AbortSignal): Promise<UniqueValuesResponse>;
