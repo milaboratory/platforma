42483455896b3b698cb557c0daf9e4fc4259daaf
Korenevskiy Denis <denkoren@gmail.com>
2025-04-08 16:34:43 +0300

    keep WDs for 20m after command success, enable CPU/RAM limit in pl-dev

--- tools/pl-bootstrap/src/templates/pl-config.ts
+++ tools/pl-bootstrap/src/templates/pl-config.ts
@@ -203,7 +203,6 @@ export function render(options: types.plSettings): string {
   const disableMon = options.monitoring.enabled ? '' : ' disabled';
   const disableDbg = options.debug.enabled ? '' : ' disabled';
   const libraryDownloadable = options.hacks.libraryDownloadable ? 'true' : 'false';
-
   let miLicenseSecret = options.license.value;
   if (options.license.file != '') {
     miLicenseSecret = fs.readFileSync(options.license.file).toString().trimEnd();
@@ -283,7 +282,12 @@ controllers:
   packageLoader:
     packagesRoot: '${options.localRoot}/packages'
 
-  workflows: {}
+  workflows:
+    features:
+      pureFutureFields: true
+      commandExpressions: true
+      workdirLimits: true
+      computeLimits: true
 `;
 }
 
