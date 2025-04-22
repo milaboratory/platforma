aaf40e87cfcbaa78bf95d19e379b2737d550a8e3
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2025-04-22 14:44:59 +0300

    Fix to dependency graph construction logic to include potential anchored dependencies as upstreams

--- lib/node/pl-middle-layer/src/model/project_model_util.test.ts
+++ lib/node/pl-middle-layer/src/model/project_model_util.test.ts
@@ -29,13 +29,17 @@ describe('simple traverse', () => {
   const pGraph1 = productionGraph(struct1, (id) => inputs.get(id) ?? null);
 
   test.each([
-    { roots: ['b2'], expectedUpstreams: ['b1'], expectedDownstreams: ['b6'] },
-    { roots: ['b4'], expectedUpstreams: ['b3'], expectedDownstreams: ['b5', 'b6'] },
-    { roots: ['b5'], expectedUpstreams: ['b3', 'b4'], expectedDownstreams: [] },
-    { roots: ['b6'], expectedUpstreams: ['b1', 'b2', 'b3', 'b4'], expectedDownstreams: [] }
-  ])('$roots', ({ roots, expectedUpstreams, expectedDownstreams }) => {
-    const upstreams = pGraph1.traverseIdsExcludingRoots('upstream', ...roots);
+    { roots: ['b2'], expectedDirectUpstreams: ['b1'], expectedDirectDownstreams: ['b6'], expectedUpstreams: ['b1'], expectedDownstreams: ['b6'] },
+    { roots: ['b4'], expectedDirectUpstreams: ['b3'], expectedDirectDownstreams: ['b5', 'b6'], expectedUpstreams: ['b3'], expectedDownstreams: ['b5', 'b6'] },
+    { roots: ['b5'], expectedDirectUpstreams: ['b3', 'b4'], expectedDirectDownstreams: [], expectedUpstreams: ['b3', 'b4'], expectedDownstreams: ['b6'] },
+    { roots: ['b6'], expectedDirectUpstreams: ['b1', 'b2', 'b3', 'b4'], expectedDirectDownstreams: [], expectedUpstreams: ['b1', 'b2', 'b3', 'b4', 'b5'], expectedDownstreams: [] }
+  ])('$roots', ({ roots, expectedDirectUpstreams, expectedDirectDownstreams, expectedUpstreams, expectedDownstreams }) => {
+    const directUpstreams = pGraph1.traverseIdsExcludingRoots('directUpstream', ...roots);
+    const directDownstreams = pGraph1.traverseIdsExcludingRoots('directDownstream', ...roots);
+    const upstreams = pGraph1.traverseIdsExcludingRoots('upstream', ...roots);  
     const downstreams = pGraph1.traverseIdsExcludingRoots('downstream', ...roots);
+    expect([...directUpstreams].sort()).toEqual(expectedDirectUpstreams.sort());
+    expect([...directDownstreams].sort()).toEqual(expectedDirectDownstreams.sort());
     expect([...upstreams].sort()).toEqual(expectedUpstreams.sort());
     expect([...downstreams].sort()).toEqual(expectedDownstreams.sort());
   });
