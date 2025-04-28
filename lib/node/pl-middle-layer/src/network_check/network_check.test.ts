7006575745a93006f2a7550ef602d98fab1a0a14
Gleb Zakharov <snyssfx@posteo.net>
2025-04-28 15:49:23 +0200

    network check: skip network check test again

--- lib/node/pl-middle-layer/src/network_check/network_check.test.ts
+++ lib/node/pl-middle-layer/src/network_check/network_check.test.ts
@@ -2,7 +2,7 @@ import { checkNetwork } from './network_check';
 import { testCredentials } from './test_utils';
 
 // it's too long, enable only when you need to see the report.
-test('should return network connectivity report for a platform endpoint set in PL_ADDRESS env', async () => {
+test.skip('should return network connectivity report for a platform endpoint set in PL_ADDRESS env', async () => {
   expect(process.env.PL_ADDRESS).not.toBeUndefined();
   expect(process.env.PL_TEST_USER).not.toBeUndefined();
   expect(process.env.PL_TEST_PASSWORD).not.toBeUndefined();
