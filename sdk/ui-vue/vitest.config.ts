73e00111e2c280062b93c091e293a7297fa73a5d
Stanislav Poslavsky <poslavskysv@milaboratories.com>
2025-05-19 23:26:44 +0200

    mock worker in test

--- sdk/ui-vue/vitest.config.ts
+++ sdk/ui-vue/vitest.config.ts
@@ -1,7 +1,16 @@
+import path from 'path';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
     includeSource: ['src/**/*.{js,ts}'],
+  },
+  resolve: {
+    alias: [
+      {
+        find: /^.*\/chemical-properties\.worker\?worker&inline$/,
+        replacement: path.resolve(__dirname, 'src/components/PlMultiSequenceAlignment/highlight/__mocks__/chemical-properties.worker.ts')
+      },
+    ],
   },
 });
