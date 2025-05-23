494cc0c7fa8f172a7a9af68d628a060e52453010
Korenevskiy Denis <denkoren@gmail.com>
2025-05-23 15:59:42 +0300

    fix: more paths transformed to be compatible with S3

--- lib/node/pl-drivers/src/drivers/ls.test.ts
+++ lib/node/pl-drivers/src/drivers/ls.test.ts
@@ -39,6 +39,8 @@ test('should ok when list files from remote storage in ls driver', async () => {
       throw Error();
     });
 
+    const universalPath = (path: string) => path.startsWith('/') ? path.slice(1) : path;
+
     const storages = await driver.getStorageList();
     const library = storages.find((se) => se.name == env.libraryStorage)!.handle;
 
@@ -49,20 +51,19 @@ test('should ok when list files from remote storage in ls driver', async () => {
     expect(testDir).toBeDefined();
     expect(testDir!.type).toEqual('dir');
 
-    const fullPath = testDir!.fullPath.startsWith('/') ? testDir!.fullPath.slice(1) : testDir!.fullPath;
-    expect(fullPath).toEqual('ls_dir_structure_test');
+    expect(universalPath(testDir!.fullPath)).toEqual('ls_dir_structure_test');
     expect(testDir!.name).toEqual('ls_dir_structure_test');
 
     const secondDirs = await driver.listFiles(library, testDir!.fullPath);
     expect(secondDirs.entries).toHaveLength(2);
     expect(secondDirs.entries[0].type).toEqual('dir');
-    expect(secondDirs.entries[0].fullPath).toEqual('/ls_dir_structure_test/abc');
+    expect(universalPath(secondDirs.entries[0].fullPath)).toEqual('ls_dir_structure_test/abc');
     expect(secondDirs.entries[0].name).toEqual('abc');
 
     const f = await driver.listFiles(library, secondDirs.entries[0].fullPath);
     expect(f.entries).toHaveLength(1);
     expect(f.entries[0].type).toEqual('file');
-    expect(f.entries[0].fullPath).toEqual('/ls_dir_structure_test/abc/42.txt');
+    expect(universalPath(f.entries[0].fullPath)).toEqual('ls_dir_structure_test/abc/42.txt');
     expect(f.entries[0].name).toEqual('42.txt');
     expect((f.entries[0] as any).handle).toContain('index://index/');
   });
