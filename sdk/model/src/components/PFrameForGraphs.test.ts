cb527eae9a8c0536d5960c73124459680c9532b5
Elena Erokhina <erohinaelena1@yandex.ru>
2025-06-06 16:45:01 +0300

    add linker chains

--- sdk/model/src/components/PFrameForGraphs.test.ts
+++ sdk/model/src/components/PFrameForGraphs.test.ts
@@ -2,10 +2,10 @@ import {test, expect, describe} from '@jest/globals';
 import {
     IS_VIRTUAL_COLUMN,
     LABEL_ANNOTATION,
-    enrichCompatible
+    enrichCompatible, LINKER_COLUMN_ANNOTATION, getAvailableWithLinkersAxes
 } from './PFrameForGraphs';
 import {
-    AxisId,
+    AxisId, CanonicalizedJson,
     canonicalizeJson, DataInfo,
     getAxisId,
     PColumn,
@@ -15,8 +15,8 @@ import {
 } from "@milaboratories/pl-model-common";
 import {TreeNodeAccessor} from "../render";
 
-function getAllAxesFromSpecs (specs:PColumnSpec[]):Map<string, AxisId> {
-    const allAxes:Map<string, AxisId> = new Map();
+function getAllAxesFromSpecs (specs:PColumnSpec[]):Map<CanonicalizedJson<AxisId>, AxisId> {
+    const allAxes:Map<CanonicalizedJson<AxisId>, AxisId> = new Map();
     for (const spec of specs) {
         for (const id of spec.axesSpec) {
             const aid = getAxisId(id);
@@ -191,5 +191,84 @@ describe('PFrameForGraph', () => {
         const resultColumns = enrichCompatible(allAxes, columns);
         expect(resultColumns[2].spec.annotations?.[LABEL_ANNOTATION]).toEqual('Label of column2 / a');
         expect(resultColumns[3].spec.annotations?.[LABEL_ANNOTATION]).toEqual('Label of column2 / b');
+    })
+
+    test('Linker column adds available axis', () => {
+        const columnSpec1: PColumnSpec = {
+            kind: 'PColumn',
+            name: 'column1',
+            valueType: 'Int',
+            axesSpec: [
+                {type: 'String', name: 'axis1', domain: {key1: 'a'}},
+                {type: 'String', name: 'axis2', domain: {key1: 'b'}},
+            ]
+        };
+        const linkerColumn13: PColumnSpec = {
+            kind: 'PColumn',
+            name: 'linker13',
+            valueType: 'String',
+            annotations: {[LINKER_COLUMN_ANNOTATION]: 'true'},
+            axesSpec: [
+                {type: 'String', name: 'axis1'},
+                {type: 'String', name: 'axis3'}
+            ]
+        }
+
+        const linkerColumns = [{id: 'id5' as PObjectId, spec: linkerColumn13, data: []}];
+        const availableAxes = getAvailableWithLinkersAxes(
+            linkerColumns,
+            getAllAxesFromSpecs([columnSpec1])
+        );
+        expect([...availableAxes.values()].map(id => id.name)).toEqual(['axis3'])
+    })
+
+    test('Linker columns add available axes by chains', () => {
+        const columnSpec1: PColumnSpec = {
+            kind: 'PColumn',
+            name: 'column1',
+            valueType: 'Int',
+            axesSpec: [{type: 'String', name: 'axis1'}]
+        };
+        const linkerColumn12: PColumnSpec = {
+            kind: 'PColumn',
+            name: 'linker12',
+            valueType: 'String',
+            annotations: {[LINKER_COLUMN_ANNOTATION]: 'true'},
+            axesSpec: [
+                {type: 'String', name: 'axis1'},
+                {type: 'String', name: 'axis2'}
+            ]
+        }
+        const linkerColumn23: PColumnSpec = {
+            kind: 'PColumn',
+            name: 'linker23',
+            valueType: 'String',
+            annotations: {[LINKER_COLUMN_ANNOTATION]: 'true'},
+            axesSpec: [
+                {type: 'String', name: 'axis2'},
+                {type: 'String', name: 'axis3'}
+            ]
+        }
+        const linkerColumn34: PColumnSpec = {
+            kind: 'PColumn',
+            name: 'linker34',
+            valueType: 'String',
+            annotations: {[LINKER_COLUMN_ANNOTATION]: 'true'},
+            axesSpec: [
+                {type: 'String', name: 'axis3'},
+                {type: 'String', name: 'axis4'}
+            ]
+        }
+
+        const linkerColumns = [
+            {id: 'id3' as PObjectId, spec: linkerColumn12, data: []},
+            {id: 'id4' as PObjectId, spec: linkerColumn23, data: []},
+            {id: 'id5' as PObjectId, spec: linkerColumn34, data: []},
+        ];
+        const availableAxes = getAvailableWithLinkersAxes(
+            linkerColumns,
+            getAllAxesFromSpecs([columnSpec1])
+        );
+        expect([...availableAxes.values()].map(id => id.name)).toEqual(['axis2', 'axis3', 'axis4'])
     })
 })
--- sdk/model/src/components/PFrameForGraphs.ts
+++ sdk/model/src/components/PFrameForGraphs.ts
@@ -1,15 +1,16 @@
 import type {
-  AxisId,
+  AxisId, CanonicalizedJson,
   DataInfo,
   PColumn,
-  PColumnSpec, PColumnValues,
+  PColumnSpec,
+  PColumnValues,
   PFrameHandle,
   PObjectId,
 } from '@milaboratories/pl-model-common';
 import {
   canonicalizeJson,
   getAxisId,
-  matchAxisId,
+  matchAxisId, parseJson,
 } from '@milaboratories/pl-model-common';
 import type { RenderCtx } from '../render';
 import { PColumnCollection, TreeNodeAccessor } from '../render';
@@ -54,6 +55,60 @@ export const IS_VIRTUAL_COLUMN = 'pl7.app/graph/isVirtual'; // annotation for co
 export const LABEL_ANNOTATION = 'pl7.app/label';
 export const LINKER_COLUMN_ANNOTATION = 'pl7.app/isLinkerColumn';
 
+export type LinkerColumnsMap = Map<CanonicalizedJson<AxisId>, Set<CanonicalizedJson<AxisId>>>;
+export function getLinkerColumnsMap(linkerColumns: PColumn<TreeNodeAccessor | DataInfo<TreeNodeAccessor> | PColumnValues>[]) {
+  const resultMap: LinkerColumnsMap = new Map();
+  for (const { spec } of linkerColumns) {
+    const axisIds = spec.axesSpec.map(getAxisId).map(canonicalizeJson);
+    axisIds.forEach((id) => {
+      if (!resultMap.has(id)) {
+        resultMap.set(id, new Set());
+      }
+    });
+    for (let i = 0; i < axisIds.length - 1; i++) {
+      for (let j = i + 1; j < axisIds.length; j++) {
+        const id1 = axisIds[i];
+        const id2 = axisIds[j];
+        resultMap.get(id1)?.add(id2);
+        resultMap.get(id2)?.add(id1);
+      }
+    }
+  }
+  return resultMap;
+}
+
+export function getAvailableWithLinkersAxes(
+  linkerColumns: PColumn<TreeNodeAccessor | DataInfo<TreeNodeAccessor> | PColumnValues>[],
+  blockAxes: Map<CanonicalizedJson<AxisId>, AxisId>,
+): Map<CanonicalizedJson<AxisId>, AxisId> {
+  const linkerColumnsMap = getLinkerColumnsMap(linkerColumns);
+  const linkerColumnsMapIds = [...linkerColumnsMap.keys()].map(parseJson);
+  const startKeys: CanonicalizedJson<AxisId>[] = [];
+  for (const startId of blockAxes.values()) {
+    const matched = linkerColumnsMapIds.find((id) => matchAxisId(startId, id));
+    if (matched) {
+      startKeys.push(canonicalizeJson(matched)); // linker column can contain fewer domains than in block's columns, it's fixed on next step in enrichCompatible
+    }
+  }
+  const visited: Set<CanonicalizedJson<AxisId>> = new Set(startKeys);
+  const addedAvailableAxes: Map<CanonicalizedJson<AxisId>, AxisId> = new Map();
+  let nextKeys = [...startKeys];
+
+  while (nextKeys.length) {
+    const next: CanonicalizedJson<AxisId>[] = [];
+    for (const nextKey of nextKeys) {
+      for (const availableKey of linkerColumnsMap.get(nextKey) ?? []) {
+        if (!visited.has(availableKey)) {
+          next.push(availableKey);
+          visited.add(availableKey);
+          addedAvailableAxes.set(availableKey, parseJson(availableKey));
+        }
+      }
+    }
+    nextKeys = next;
+  }
+  return addedAvailableAxes;
+}
 /** Add columns with fully compatible axes created from partial compatible ones */
 export function enrichCompatible(blockAxes: Map<string, AxisId>, columns: PColumn<TreeNodeAccessor | DataInfo<TreeNodeAccessor> | PColumnValues>[]) {
   const result: PColumn<TreeNodeAccessor | DataInfo<TreeNodeAccessor> | PColumnValues>[] = [];
@@ -160,7 +215,7 @@ export function createPFrameForGraphs<A, U>(
   columns.addColumns(blockColumns as PColumn<TreeNodeAccessor | DataInfo<TreeNodeAccessor>>[]);
 
   // all possible axes from block columns
-  const allAxes = new Map<string, AxisId>();
+  const allAxes = new Map<CanonicalizedJson<AxisId>, AxisId>();
   for (const c of blockColumns) {
     for (const id of c.spec.axesSpec) {
       const aid = getAxisId(id);
@@ -169,17 +224,13 @@ export function createPFrameForGraphs<A, U>(
   }
 
   // all linker columns always go to pFrame - even it's impossible to use some of them they all are hidden
-  const linkerColumns = columns.getColumns(
-    (spec) => isLinkerColumn(spec)
-      && spec.axesSpec.some((ax) => allAxes.has(canonicalizeJson(getAxisId(ax)))), // TODO: linkers chains missed
-  ) ?? [];
+  const linkerColumns = columns.getColumns((spec) => isLinkerColumn(spec)) ?? [];
+  const availableWithLinkersAxes = getAvailableWithLinkersAxes(linkerColumns, allAxes);
   const labelColumns = columns.getColumns(isLabelColumn) ?? [];
+
   // all possible axes from connected linkers
-  for (const c of linkerColumns) {
-    for (const id of c.spec.axesSpec) {
-      const aid = getAxisId(id);
-      allAxes.set(canonicalizeJson(aid), aid);
-    }
+  for (const item of availableWithLinkersAxes) {
+    allAxes.set(...item);
   }
 
   let compatible = (columns.getColumns([...allAxes.values()]
