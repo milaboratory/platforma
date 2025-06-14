544c5912825fe82660f2ad0d8e1ab6d540c49f16
Alexandr <hellbeast92@gmail.com>
2025-06-14 19:33:35 +0200

    fix: more canonical vue approach + fix bug error on pin after removed pinned items

--- sdk/ui-vue/src/components/PlElementList/utils.ts
+++ sdk/ui-vue/src/components/PlElementList/utils.ts
@@ -1,3 +1,6 @@
+import { isRef, isShallow } from 'vue';
+import { shallowClone } from '@milaboratories/helpers';
+
 export const moveElements = <T>(array: T[], from: number, to: number): T[] => {
   if (to >= 0 && to < array.length) {
     const element = array.splice(from, 1)[0];
@@ -6,3 +9,9 @@ export const moveElements = <T>(array: T[], from: number, to: number): T[] => {
 
   return array;
 };
+
+export function optionalUpdateRef<T>(ref: T) {
+  if (isRef(ref) && isShallow(ref)) {
+    ref.value = shallowClone(ref);
+  }
+}
