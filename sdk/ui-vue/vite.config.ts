a051da5e750e8bb918f8f9a43b8cc2bb31a0b3da
Alexandr <hellbeast92@gmail.com>
2025-06-16 18:37:54 +0200

    fix: correct place for rollupOptions

--- sdk/ui-vue/vite.config.ts
+++ sdk/ui-vue/vite.config.ts
@@ -8,20 +8,20 @@ export default defineConfig((configEnv): UserConfig => {
   return mergeConfig(createViteLibConfig(configEnv), {
     build: {
       lib: { name: 'SdkVueLib', entry: resolve(__dirname, 'src/lib.ts') },
+      rollupOptions: {
+        external: [
+          'vue',
+          'ag-grid-enterprise',
+          'ag-grid-vue3',
+          '@milaboratories/biowasm-tools',
+          '@milaboratories/miplots4'
+        ],
+        output: {
+          globals: {
+            vue: 'Vue'
+          }
+        }
+      }
     },
-    rollupOptions: {
-      external: [
-        'vue',
-        'ag-grid-enterprise',
-        'ag-grid-vue3',
-        '@milaboratories/biowasm-tools',
-        '@milaboratories/miplots4',
-      ],
-      output: {
-        globals: {
-          vue: 'Vue',
-        },
-      },
-    },
-  });
+  } satisfies UserConfig);
 });
