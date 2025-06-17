ae822c0e88fb85538f120ea1d4e3e2bf0e88d161
Alexandr <hellbeast92@gmail.com>
2025-06-17 14:20:15 +0200

    feat: refactor everything that related to ui-vue package

--- etc/ui-vue-playground/vite.config.ts
+++ etc/ui-vue-playground/vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
+import { defineConfig, mergeConfig } from 'vite';
+import { createViteDevConfig } from '@milaboratories/platforma-build-configs';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig((configEnv) => {
+  return mergeConfig(createViteDevConfig(configEnv), {});
 });
--- etc/uikit-playground/vite.config.ts
+++ etc/uikit-playground/vite.config.ts
@@ -1,17 +1,17 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'url';
+import { defineConfig, mergeConfig } from 'vite';
+import { createViteDevConfig } from '@milaboratories/platforma-build-configs';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-  define: {
-    APP_VERSION: JSON.stringify('unknown'), // @todo
-  },
-  resolve: {
-    alias: {
-      'milaboratories/uikit/styles': fileURLToPath(new URL('../../lib/ui/uikit/src/assets/ui.scss', import.meta.url)),
-      'milaboratories/uikit': fileURLToPath(new URL('../../lib/ui/uikit/src/index.ts', import.meta.url)),
+export default defineConfig((configEnv) => {
+  return mergeConfig(createViteDevConfig(configEnv), {
+    define: {
+      APP_VERSION: JSON.stringify('unknown'), // @todo
     },
-  },
+    resolve: {
+      alias: {
+        'milaboratories/uikit/styles': fileURLToPath(new URL('../../lib/ui/uikit/src/assets/ui.scss', import.meta.url)),
+        'milaboratories/uikit': fileURLToPath(new URL('../../lib/ui/uikit/src/index.ts', import.meta.url)),
+      },
+    },
+  });
 });
