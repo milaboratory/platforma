23f719d1d9d76d794eb3abdfca837798439d8065
Elena Erokhina <erohinaelena1@yandex.ru>
2025-06-18 18:29:56 +0300

    MILAB-2864: add description for createPFrameForGraphs (#897)

--- sdk/model/src/components/PFrameForGraphs.ts
+++ sdk/model/src/components/PFrameForGraphs.ts
@@ -203,6 +203,18 @@ function getAdditionalColumnsForColumn(
   return [column, ...additionalColumns];
 }
 
+/**
+ The aim of createPFrameForGraphs: to create pframe with block’s columns and all compatible columns from result pool
+ (including linker columns and all label columns).
+ Block’s columns are added to pframe as is.
+ Other columns are added basing on set of axes of block’s columns, considering available with linker columns.
+ Compatible columns must have at least one axis from block’s axes set. This axis of the compatible column from
+ result pool must satisfy matchAxisId (it can have less domain keys than in block’s axis, but without conflicting values
+ among existing ones).
+ In requests to pframe (calculateTableData) columns must have strictly the same axes. For compatibility in case
+ of partially matched axis we add to pframe a copy of this column with modified axis (with filled missed domains)
+ and modified label (with added domain values in case if more than one copy with different domains exist).
+ */
 export function createPFrameForGraphs<A, U>(
   ctx: RenderCtx<A, U>,
   blockColumns: PColumn<PColumnDataUniversal>[] | undefined,
