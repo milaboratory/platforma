87bb3ca59a3bddf0f23bbc22ce0d7e0e746d4956
Vadim Piven <vadim.piven@milaboratories.com>
2025-06-19 14:04:18 +0200

    PFrames make getShape truly asynchronous

--- lib/model/middle-layer/src/pframe/internal_api/pframe.ts
+++ lib/model/middle-layer/src/pframe/internal_api/pframe.ts
@@ -1,5 +1,5 @@
 import type { PFrameFactoryAPI } from './api_factory';
-import type { PFrameReadAPIV6 } from './api_read';
+import type { PFrameReadAPIV6, PFrameReadAPIV7 } from './api_read';
 
 export type Logger = (
   level: 'info' | 'warn' | 'error',
@@ -7,3 +7,5 @@ export type Logger = (
 ) => void;
 
 export interface PFrameV6 extends PFrameFactoryAPI, PFrameReadAPIV6 {}
+
+export interface PFrameV7 extends PFrameFactoryAPI, PFrameReadAPIV7 {}
