656a1fc9bda8d9f44d1d9b425dbf06999a652225
Alexandr <hellbeast92@gmail.com>
2025-06-24 18:37:48 +0200

    fix: remove composite flag from ts config. we don't use it

--- tools/build-configs/src/createViteLibConfig.ts
+++ tools/build-configs/src/createViteLibConfig.ts
@@ -1,27 +1,33 @@
 import type { ConfigEnv } from 'vite';
 import { mergeConfig } from 'vite';
 import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js'
+import dts from 'vite-plugin-dts';
 import { createViteDevConfig } from './createViteDevConfig';
 
 export const createViteLibConfig = ((configEnv: ConfigEnv) => {
-    return mergeConfig(createViteDevConfig(configEnv), {
-      plugins: [cssInjectedByJsPlugin({ relativeCSSInjection: true })],
-      build: {
-        lib: {
-          fileName: 'lib',
-          formats: ['es'],
-        },
-        cssCodeSplit: true,
-        rollupOptions: {
-          output: {
-            preserveModules: true,
-            preserveModulesRoot: 'src',
-            inlineDynamicImports: false,
-            entryFileNames: '[name].js',
-            chunkFileNames: '[name]-[hash].js',
-            assetFileNames: '[name][extname]'
-          }
-        },
+  const isProd = configEnv.mode === 'production';
+
+  return mergeConfig(createViteDevConfig(configEnv), {
+    plugins: [
+      dts({ tsconfigPath: isProd ? 'tsconfig.lib.build.json' : undefined }),
+      cssInjectedByJsPlugin({ relativeCSSInjection: true }),
+    ],
+    build: {
+      lib: {
+        fileName: 'lib',
+        formats: ['es'],
       },
-    });
-  })
+      cssCodeSplit: true,
+      rollupOptions: {
+        output: {
+          preserveModules: true,
+          preserveModulesRoot: 'src',
+          inlineDynamicImports: false,
+          entryFileNames: '[name].js',
+          chunkFileNames: '[name]-[hash].js',
+          assetFileNames: '[name][extname]'
+        }
+      },
+    },
+  });
+})
