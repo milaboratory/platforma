8ba07d43b81b4df07e9cd8df9cacebbf54e7bdaf
Vadim Piven <vadim.piven@milaboratories.com>
2025-07-04 14:10:40 +0200

    PFrame driver: filter & sort without rejoining, LRU cache for graphics

--- lib/model/middle-layer/src/pframe/internal_api/pframe.ts
+++ lib/model/middle-layer/src/pframe/internal_api/pframe.ts
@@ -1,5 +1,5 @@
 import type { PFrameFactoryAPI } from './api_factory';
-import type { PFrameReadAPIV7 } from './api_read';
+import type { PFrameReadAPIV7, PFrameReadAPIV8 } from './api_read';
 
 export type Logger = (
   level: 'info' | 'warn' | 'error',
@@ -7,3 +7,5 @@ export type Logger = (
 ) => void;
 
 export interface PFrameV7 extends PFrameFactoryAPI, PFrameReadAPIV7 {}
+
+export interface PFrameV8 extends PFrameFactoryAPI, PFrameReadAPIV8 {}
