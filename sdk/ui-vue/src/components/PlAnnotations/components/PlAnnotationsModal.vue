6d3cb161308b70f4515a30f13b084ce038df212d
Alexandr <hellbeast92@gmail.com>
2025-07-07 14:31:04 +0200

    feat: annotation creation dialog

--- sdk/ui-vue/src/components/PlAnnotations/components/PlAnnotationsModal.vue
+++ sdk/ui-vue/src/components/PlAnnotations/components/PlAnnotationsModal.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
-import { shallowRef } from 'vue';
-import { PlSidebarGroup, PlPureSlideModal } from '@milaboratories/uikit';
+import { PlPureSlideModal, PlSidebarGroup } from '@milaboratories/uikit';
 import type { AnnotationScriptUi } from '@platforma-sdk/model';
-import type { SimplifiedUniversalPColumnEntry } from './types';
-import { getDefaultAnnotationScript } from './utils';
+import { computed, effect, shallowRef } from 'vue';
+import type { SimplifiedUniversalPColumnEntry } from '../types';
+import { getDefaultAnnotationScript } from '../utils';
 import AnnotationsSidebar from './AnnotationsSidebar.vue';
 import FilterSidebar from './FilterSidebar.vue';
+import PlAnnotationCreateDialog from './PlAnnotationCreateDialog.vue';
 
 // Models
 const annotation = defineModel<AnnotationScriptUi>('annotation', { required: true, default: getDefaultAnnotationScript });
@@ -14,10 +15,34 @@ const opened = defineModel<boolean>('opened', { required: true });
 const props = defineProps<{ columns: SimplifiedUniversalPColumnEntry[] }>();
 // State
 const selectedStepId = shallowRef<number | undefined>(undefined);
+const hasAnnotation = computed(() => annotation.value.isCreated === true);
+
+const openedDialog = computed({
+  get: () => !hasAnnotation.value && opened.value,
+  set: (value: boolean) => (opened.value = value),
+});
+const openedModal = computed({
+  get: () => hasAnnotation.value && opened.value,
+  set: (value: boolean) => (opened.value = value),
+});
+
+effect(() => {
+  if (selectedStepId.value === undefined && annotation.value.steps.length > 0) {
+    selectedStepId.value = annotation.value.steps[0].id;
+  }
+});
+
+function handleCreateAnnotation(props: { type: 'byClonotype' | 'bySampleAndClonotype'; name: string }) {
+  annotation.value.isCreated = true;
+  annotation.value.mode = props.type;
+  annotation.value.title = props.name;
+  annotation.value.steps = [];
+}
 </script>
 
 <template>
-  <PlPureSlideModal ref="modal" v-model="opened" :class="$style.modal" width="768px">
+  <PlAnnotationCreateDialog v-model="openedDialog" @submit="handleCreateAnnotation"/>
+  <PlPureSlideModal v-model="openedModal" :class="$style.modal" width="768px">
     <PlSidebarGroup :class="$style.sidebarGroup">
       <template #item-0>
         <AnnotationsSidebar
