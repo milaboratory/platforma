ac1895e98136cbb746b2cfd01a3f4afe06778f38
Alexandr <hellbeast92@gmail.com>
2025-07-08 16:13:58 +0200

    ref: remove unused migration function and dependency

--- sdk/ui-vue/src/components/PlAnnotations/annotationModelController.ts
+++ sdk/ui-vue/src/components/PlAnnotations/annotationModelController.ts
@@ -1,15 +1,11 @@
 import type { AnnotationScript, AnnotationScriptUi } from '@platforma-sdk/model';
 import { compileAnnotationScript } from '@platforma-sdk/model';
 import { watchDebounced } from '@vueuse/core';
-import { migratgeToWithId } from './utils';
 
 export function annotationModelController(
   getArgsState: () => AnnotationScript,
   getUiState: () => AnnotationScriptUi,
 ) {
-  migratgeToWithId(getUiState().steps);
-  getUiState().steps.forEach((step) => migratgeToWithId(step.filter.filters));
-
   watchDebounced(getUiState, (value) => {
     try {
       Object.assign(getArgsState(), compileAnnotationScript(value));
