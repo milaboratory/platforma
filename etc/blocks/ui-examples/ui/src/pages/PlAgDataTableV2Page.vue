6540ce214e9d2b1707b7db01ff7fc60bec9d685e
Kevin Detry <206546615+kevindetry-milaboratories@users.noreply.github.com>
2025-07-10 11:22:49 +0200

    [ui-examples] Add PlAgDataTableV2 reactive text examples (#953)

--- etc/blocks/ui-examples/ui/src/pages/PlAgDataTableV2Page.vue
+++ etc/blocks/ui-examples/ui/src/pages/PlAgDataTableV2Page.vue
@@ -1,31 +1,24 @@
 <script setup lang="ts">
+import { isJsonEqual } from '@milaboratories/helpers';
+import { matchAxisId, type PlSelectionModel } from '@platforma-sdk/model';
 import {
-  computed,
-  ref,
-  watch,
-  toValue,
-} from 'vue';
-import {
-  isJsonEqual,
-} from '@milaboratories/helpers';
-import {
-  PlBlockPage,
   PlAgDataTableV2,
-  PlNumberField,
+  PlBlockPage,
   PlCheckbox,
   PlDropdown,
+  PlNumberField,
   usePlDataTableSettingsV2,
 } from '@platforma-sdk/ui-vue';
+import type { ICellRendererParams } from 'ag-grid-enterprise';
 import {
-  useApp,
-} from '../app';
-import {
-  matchAxisId,
-  type PlSelectionModel,
-} from '@platforma-sdk/model';
-import type {
-  ICellRendererParams,
-} from 'ag-grid-enterprise';
+  computed,
+  onWatcherCleanup,
+  ref,
+  toValue,
+  watch,
+  watchEffect,
+} from 'vue';
+import { useApp } from '../app';
 
 const app = useApp();
 
@@ -37,9 +30,11 @@ const sources = [...new Array(10)].map((_, i) => {
 });
 const activeSource = ref(sources[0].value);
 
+const loading = ref(false);
+
 const tableSettings = usePlDataTableSettingsV2({
-  sourceId: () => activeSource.value,
-  model: () => app.model.outputs.ptV2,
+  sourceId: () => loading.value ? 'loading_source' : activeSource.value,
+  model: () => loading.value ? undefined : app.model.outputs.ptV2,
   sheets: () => app.model.outputs.ptV2Sheets,
   filtersConfig: ({ sourceId, column }) => {
     if (isJsonEqual(sourceId, sources[0].value)) {
@@ -58,7 +53,10 @@ const tableSettings = usePlDataTableSettingsV2({
       }
     }
     if (isJsonEqual(sourceId, sources[1].value)) {
-      if (column.type === 'axis' && matchAxisId({ type: 'Int', name: 'index' }, column.id)) {
+      if (
+        column.type === 'axis'
+        && matchAxisId({ type: 'Int', name: 'index' }, column.id)
+      ) {
         return {
           default: {
             type: 'number_lessThanOrEqualTo',
@@ -72,6 +70,7 @@ const tableSettings = usePlDataTableSettingsV2({
 });
 
 const verbose = ref(false);
+
 const cellRendererSelector = computed(() => {
   if (!verbose.value) return;
   return (params: ICellRendererParams) => {
@@ -87,6 +86,35 @@ const selection = ref<PlSelectionModel>({
   axesSpec: [],
   selectedKeys: [],
 });
+
+const reactiveText = ref(false);
+
+const now = ref(new Date());
+const timeFormatter = new Intl.DateTimeFormat('en', { timeStyle: 'medium' });
+
+const reactiveTextProps = computed(() => {
+  if (!reactiveText.value) return;
+  const formattedNow = timeFormatter.format(now.value);
+  return {
+    loadingText: 'Loading at ' + formattedNow,
+    notReadyText: 'Not ready at ' + formattedNow,
+    noRowsText: 'No rows at ' + formattedNow,
+  };
+});
+
+watchEffect(() => {
+  if (!reactiveText.value) return;
+  let animationFrame = requestAnimationFrame(function tick() {
+    now.value = new Date();
+    animationFrame = requestAnimationFrame(tick);
+  });
+  onWatcherCleanup(() => {
+    if (typeof animationFrame !== 'undefined') {
+      cancelAnimationFrame(animationFrame);
+    }
+  });
+});
+
 watch(
   () => selection.value,
   (selection) => console.log(`selection changed`, toValue(selection)),
@@ -101,11 +129,16 @@ watch(
       v-model:selection="selection"
       :settings="tableSettings"
       :cell-renderer-selector="cellRendererSelector"
+      v-bind="reactiveTextProps"
       show-export-button
     >
       <template #before-sheets>
         <PlNumberField v-model="app.model.args.tableNumRows" />
-        <PlCheckbox v-model="verbose">Use custom cell renderer for numbers</PlCheckbox>
+        <PlCheckbox v-model="verbose">
+          Use custom cell renderer for numbers
+        </PlCheckbox>
+        <PlCheckbox v-model="loading">Infinite loading</PlCheckbox>
+        <PlCheckbox v-model="reactiveText">Reactive text</PlCheckbox>
         <PlDropdown v-model="activeSource" :options="sources" clearable />
       </template>
     </PlAgDataTableV2>
