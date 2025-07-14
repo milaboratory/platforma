5d21b369d3a24c530ab314c436655c3e389ac7f1
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2025-07-14 21:19:39 +0300

    New caching features in workflow SDK (#967)

---
+++ tests/workflow-tengo/src/anonymize/anonymize.test.ts
@@ -0,0 +1,119 @@
+import { field, Pl } from '@milaboratories/pl-middle-layer';
+import { tplTest } from '@platforma-sdk/test';
+
+tplTest.concurrent('anonymizeFields simple test', async ({ helper, expect }) => {
+  const renderAnonymize = async (target: Record<string, string>) => {
+    const result = await helper.renderTemplate(
+      true,
+      'anonymize.apply-anonymize-fields',
+      ['result'],
+      (tx) => {
+        const targetRef = tx.createStruct({ name: 'Test', version: '1' });
+        for (const [key, value] of Object.entries(target)) {
+          tx.createField(field(targetRef, key), 'Input', tx.createValue(Pl.JsonObject, JSON.stringify({ value })));
+        }
+        tx.lock(targetRef);
+        return {
+          target: targetRef,
+          params: tx.createJsonValue({}),
+        };
+      },
+    );
+    return result.computeOutput('result', (a) => a?.listInputFields());
+  };
+
+  const values = {
+    valA: 'value-a',
+    valB: 'value-b',
+  };
+
+  const target1 = {
+    field1: values.valA,
+    field2: values.valB,
+  };
+
+  const target2 = {
+    anotherField1: values.valA,
+    anotherField2: values.valB,
+  };
+
+  const result1Fields = await (await renderAnonymize(target1)).awaitStableValue();
+  const result2Fields = await (await renderAnonymize(target2)).awaitStableValue();
+
+  expect(result1Fields).toBeDefined();
+  expect(result2Fields).toBeDefined();
+  expect(result1Fields!.sort()).toEqual(result2Fields!.sort());
+});
+
+tplTest.concurrent('anonymizePKeys simple test', async ({ helper, expect }) => {
+  const renderAnonymizePKeys = async (target: Record<string, unknown>, pKeyIndices: number[]) => {
+    const result = await helper.renderTemplate(
+      true,
+      'anonymize.apply-anonymize-pkeys',
+      ['result', 'mapping', 'deanonymizeResult'],
+      (tx) => {
+        const targetRef = tx.createStruct({ name: 'TestPkeys', version: '1' });
+        for (const [key, value] of Object.entries(target)) {
+          tx.createField(field(targetRef, key), 'Input', tx.createValue(Pl.JsonObject, JSON.stringify(value)));
+        }
+        tx.lock(targetRef);
+        return {
+          target: targetRef,
+          params: tx.createValue(Pl.JsonObject, JSON.stringify({ pKeyIndices, originalKeyLength: 3 })),
+        };
+      },
+    );
+    return {
+      result: result.computeOutput('result', (a) => a?.listInputFields()),
+      deanonymizeResult: result.computeOutput('deanonymizeResult', (a) => a?.listInputFields()),
+      mapping: result.computeOutput('mapping', (a) => a?.getDataAsJson<Record<string, string>>()),
+    };
+  };
+
+  const values = {
+    valA: 'value-a',
+    valB: 'value-b',
+  };
+
+  const createPColumnFields = (keyTuple: string[], value: string, index: string) => {
+    const key = JSON.stringify(keyTuple);
+    return {
+      [`${key}.data`]: value,
+      [`${key}.index`]: index,
+    };
+  };
+
+  const target1 = {
+    ...createPColumnFields(['user1', 'common', 'p1'], values.valA, 'index-a'),
+    ...createPColumnFields(['user1', 'common', 'p2'], values.valB, 'index-b'),
+  };
+
+  const target2 = {
+    ...createPColumnFields(['user2', 'common', 'p1'], values.valA, 'index-a'),
+    ...createPColumnFields(['user2', 'common', 'p2'], values.valB, 'index-b'),
+  };
+
+  const pKeyIndicesToAnonymize = [0];
+
+  const anonymizeResult1 = await renderAnonymizePKeys(target1, pKeyIndicesToAnonymize);
+  const anonymizeResult2 = await renderAnonymizePKeys(target2, pKeyIndicesToAnonymize);
+
+  const result1Fields = await anonymizeResult1.result.awaitStableValue();
+  const result2Fields = await anonymizeResult2.result.awaitStableValue();
+
+  const mapping1 = await anonymizeResult1.mapping.awaitStableValue();
+  const mapping2 = await anonymizeResult2.mapping.awaitStableValue();
+
+  expect(Object.keys(mapping1!)[0]).toMatch(/.*-0/);
+  expect(Object.keys(mapping2!)[0]).toMatch(/.*-0/);
+
+  expect(result1Fields).toBeDefined();
+  expect(result2Fields).toBeDefined();
+  expect(result1Fields!.sort()).toEqual(result2Fields!.sort());
+
+  const deanonymizeResult1 = await anonymizeResult1.deanonymizeResult.awaitStableValue();
+  const deanonymizeResult2 = await anonymizeResult2.deanonymizeResult.awaitStableValue();
+
+  expect(deanonymizeResult1!.sort()).toEqual(Object.keys(target1).sort());
+  expect(deanonymizeResult2!.sort()).toEqual(Object.keys(target2).sort());
+});
