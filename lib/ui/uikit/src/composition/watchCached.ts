4323a1dd5bc3fe6cfb23f040e69d2827f48545be
Vadim Piven <vadim.piven@milaboratories.com>
2025-07-14 18:21:13 +0200

    computedCached moved to uikit, watchCached implemented

---
+++ lib/ui/uikit/src/composition/watchCached.ts
@@ -0,0 +1,45 @@
+import { isJsonEqual } from '@milaboratories/helpers';
+import {
+  ref,
+  watch,
+  type WatchCallback,
+  type WatchHandle,
+  type WatchSource,
+} from 'vue';
+
+type MaybeUndefined<T, I> = I extends true ? T | undefined : T;
+export interface WatchCachedOptions<Immediate = boolean> {
+  immediate?: Immediate;
+  deep?: boolean;
+  // when `once` is needed, caching is useless, use plain watch instead
+}
+export function watchCached<T, Immediate extends Readonly<boolean> = false>(
+  source: WatchSource<T>,
+  cb: WatchCallback<T, MaybeUndefined<T, Immediate>>,
+  options?: WatchCachedOptions<Immediate>,
+): WatchHandle {
+  const cachedValue = ref<T>();
+  const handle = watch(
+    source,
+    (newValue) => {
+      if (!isJsonEqual(newValue, cachedValue.value)) {
+        cachedValue.value = newValue;
+      }
+    },
+    {
+      deep: options?.deep,
+      immediate: true, // always initialize cachedValue
+    },
+  );
+  watch<T, Immediate>(
+    () => cachedValue.value!, // `!` is safe as we always initialize cachedValue
+    cb, // separate watch so that `onWatcherCleanup` would only be triggerred here
+    {
+      // standard vue `WatchOptions` conform to `WatchCachedOptions` interface,
+      // so construct new options to remove unsupported entries
+      deep: options?.deep,
+      immediate: options?.immediate,
+    },
+  );
+  return handle; // first handle is returned as stopping it would garbage collect the second one
+}
