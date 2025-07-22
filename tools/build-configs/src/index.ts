be24ec2e12a0996eb36bb1be60fdc7be0c49ead0
Alexandr <hellbeast92@gmail.com>
2025-07-22 19:17:46 +0200

    don't alias new method to deprecated

--- tools/build-configs/src/index.ts
+++ tools/build-configs/src/index.ts
@@ -2,8 +2,3 @@ export * from './createRollupBlockModelConfig';
 export * from './createRollupNodeConfig';
 export * from './createViteDevConfig';
 export * from './createViteLibConfig';
-
-// Temporary alias for backward compatibility - deprecated
-// TODO: Remove after migration is complete
-export { createRollupNodeConfig as PlViteStdNode } from './createRollupNodeConfig';
-
