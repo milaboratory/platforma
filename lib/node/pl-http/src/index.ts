25c53e20faa9eae513ddde39b3a6ad395e9b2cab
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2025-08-03 23:23:56 +0200

    Remove maxRedirections option from defaultHttpDispatcher configuration

--- lib/node/pl-http/src/index.ts
+++ lib/node/pl-http/src/index.ts
@@ -7,7 +7,6 @@ export function defaultHttpDispatcher(httpProxy?: string): Dispatcher {
     headersTimeout: 15e3,
     keepAliveTimeout: 15e3,
     keepAliveMaxTimeout: 60e3,
-    maxRedirections: 10,
   };
 
   const dispatcher = httpProxy !== undefined ? new ProxyAgent({ uri: httpProxy, ...httpOptions }) : new Agent(httpOptions);
