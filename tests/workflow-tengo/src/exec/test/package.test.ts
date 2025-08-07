c688aac6885d18af1dc6239f84da3097daa21673
Korenevskiy Denis <denkoren@gmail.com>
2025-08-07 18:46:35 +0300

    make CI tests to work with modern backend that does not pass soft meta

--- tests/workflow-tengo/src/exec/test/package.test.ts
+++ tests/workflow-tengo/src/exec/test/package.test.ts
@@ -5,19 +5,15 @@ tplTest.concurrent('package-loads-and-installs', async ({ helper, expect }) => {
   const result = await helper.renderTemplate(
     false,
     'exec.test.pkg.pkg-install',
-    ['installed', 'descriptor'],
+    ['installed'],
     (tx) => ({}),
   );
   const installedOutput = result.computeOutput('installed', (a) => a?.getDataAsJson());
-  const descriptorOutput = result.computeOutput('descriptor', (a) => a?.getDataAsJson());
 
   const installed = await installedOutput.awaitStableValue();
-  const descriptor = await descriptorOutput.awaitStableValue();
 
   expect(installed).toHaveProperty('origin');
   expect(installed).toHaveProperty('path');
-
-  expect(descriptor).toHaveProperty('binary');
 });
 
 tplTest.concurrent('pkg-file-is-exported', async ({ helper, expect, driverKit }) => {
