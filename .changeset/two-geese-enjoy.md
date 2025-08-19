7cba53f87c7c1fca799b627b6f0e1cab1e8193d5
Vitalii Popov <vpopov@milaboratories.com>
2025-08-19 13:28:51 +0200

    fix import for crc32 (#1085)
    Co-authored-by: Vitalii Popov <v.popov@millaboratories.com>
    Co-authored-by: Alexandr <hellbeast92@gmail.com>

---
+++ .changeset/two-geese-enjoy.md
@@ -0,0 +1,5 @@
+---
+'@milaboratories/pl-drivers': patch
+---
+
+package doesn't build with npm
--- lib/node/pl-drivers/src/clients/upload.ts
+++ lib/node/pl-drivers/src/clients/upload.ts
@@ -3,12 +3,12 @@ import { addRTypeToMetadata } from '@milaboratories/pl-client';
 import type { ResourceInfo } from '@milaboratories/pl-tree';
 import type { MiLogger } from '@milaboratories/ts-helpers';
 import type { RpcOptions } from '@protobuf-ts/runtime-rpc';
-import { calculate as crc32c } from 'fast-crc32c';
 import * as fs from 'node:fs/promises';
 import type { Dispatcher } from 'undici';
 import { request } from 'undici';
 import { uploadapi_ChecksumAlgorithm, type uploadapi_GetPartURL_Response } from '../proto/github.com/milaboratory/pl/controllers/shared/grpc/uploadapi/protocol';
 import { UploadClient } from '../proto/github.com/milaboratory/pl/controllers/shared/grpc/uploadapi/protocol.client';
+import { crc32c } from './crc32c';
 
 import type { IncomingHttpHeaders } from 'undici/types/header';
 
