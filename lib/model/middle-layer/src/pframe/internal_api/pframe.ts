3bb6b2b62a87879b9ebe804013ca836b499b4ea9
Vadim Piven <vadim.piven@milaboratories.com>
2025-08-21 20:02:10 +0200

    PFrames version bump

--- lib/model/middle-layer/src/pframe/internal_api/pframe.ts
+++ lib/model/middle-layer/src/pframe/internal_api/pframe.ts
@@ -1,10 +1,10 @@
-import type { PFrameFactoryAPIV2, PFrameFactoryAPIV3 } from './api_factory';
-import type { PFrameReadAPIV8, PFrameReadAPIV9 } from './api_read';
+import type { PFrameFactoryAPIV3, PFrameFactoryAPIV4 } from './api_factory';
+import type { PFrameReadAPIV9 } from './api_read';
 import type { Logger } from './common';
 
-export interface PFrameV9 extends PFrameFactoryAPIV2, PFrameReadAPIV8 {}
-
 export interface PFrameV10 extends PFrameFactoryAPIV3, PFrameReadAPIV9 {}
+
+export interface PFrameV11 extends PFrameFactoryAPIV4, PFrameReadAPIV9 {}
 
 export type PFrameOptions = {
   /** Path to directory where PFrame can create temporary files */
@@ -20,6 +20,24 @@ export interface PFrameFactory {
    * @warning Use concurrency limiting to avoid OOM crashes when multiple instances are simultaneously in use.
    */
   createPFrame(options: PFrameOptions): PFrameV10;
+
+  /**
+   * Dump active allocations from all PFrames instances in pprof format.
+   * The result of this function should be saved as `profile.pb.gz`.
+   * Use {@link https://pprof.me/} or {@link https://www.speedscope.app/}
+   * to view the allocation flamechart.
+   * @warning This method will always reject on Windows!
+   */
+  pprofDump: () => Promise<Uint8Array>;
+};
+
+/** List of PFrame management functions exposed by PFrame module */
+export interface PFrameFactoryV2 {
+  /**
+   * Create a new PFrame instance.
+   * @warning Use concurrency limiting to avoid OOM crashes when multiple instances are simultaneously in use.
+   */
+  createPFrame(options: PFrameOptions): PFrameV11;
 
   /**
    * Dump active allocations from all PFrames instances in pprof format.
