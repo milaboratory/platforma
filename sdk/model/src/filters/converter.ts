bd881ac4768ad6074daf58fa5873c7b9798c5488
Alexandr <hellbeast92@gmail.com>
2025-09-09 19:25:32 +0200

    annotations workflow sdk

--- sdk/model/src/filters/converter.ts
+++ sdk/model/src/filters/converter.ts
@@ -1,96 +1,96 @@
 import { and, col, lit, or, rank, type Expression, type ExpressionImpl } from '@milaboratories/ptabler-js';
 import type { FilterUi } from '../filters';
 
-export function convertFilterUiToExpressionImpl(ui: FilterUi): ExpressionImpl {
-  if (ui.type === 'or') {
-    const expressions = ui.filters.filter((f) => f.type !== undefined).map(convertFilterUiToExpressionImpl);
+export function convertFilterUiToExpressionImpl(value: FilterUi): ExpressionImpl {
+  if (value.type === 'or') {
+    const expressions = value.filters.filter((f) => f.type !== undefined).map(convertFilterUiToExpressionImpl);
     if (expressions.length === 0) {
       throw new Error('OR filter requires at least one operand');
     }
     return or(...expressions);
   }
 
-  if (ui.type === 'and') {
-    const expressions = ui.filters.filter((f) => f.type !== undefined).map(convertFilterUiToExpressionImpl);
+  if (value.type === 'and') {
+    const expressions = value.filters.filter((f) => f.type !== undefined).map(convertFilterUiToExpressionImpl);
     if (expressions.length === 0) {
       throw new Error('AND filter requires at least one operand');
     }
     return and(...expressions);
   }
 
-  if (ui.type === 'not') {
-    return convertFilterUiToExpressionImpl(ui.filter).not();
+  if (value.type === 'not') {
+    return convertFilterUiToExpressionImpl(value.filter).not();
   }
 
-  if (ui.type === 'isNA') {
-    return col(ui.column).isNull();
+  if (value.type === 'isNA') {
+    return col(value.column).isNull();
   }
 
-  if (ui.type === 'isNotNA') {
-    return col(ui.column).isNotNull();
+  if (value.type === 'isNotNA') {
+    return col(value.column).isNotNull();
   }
 
-  if (ui.type === 'patternEquals') {
-    return col(ui.column).eq(lit(ui.value));
+  if (value.type === 'patternEquals') {
+    return col(value.column).eq(lit(value.value));
   }
 
-  if (ui.type === 'patternNotEquals') {
-    return col(ui.column).neq(lit(ui.value));
+  if (value.type === 'patternNotEquals') {
+    return col(value.column).neq(lit(value.value));
   }
 
-  if (ui.type === 'patternContainSubsequence') {
-    return col(ui.column).strContains(ui.value, false, true);
+  if (value.type === 'patternContainSubsequence') {
+    return col(value.column).strContains(value.value, false, true);
   }
 
-  if (ui.type === 'patternNotContainSubsequence') {
-    return col(ui.column).strContains(ui.value, false, true).not();
+  if (value.type === 'patternNotContainSubsequence') {
+    return col(value.column).strContains(value.value, false, true).not();
   }
 
-  if (ui.type === 'lessThan') {
-    return col(ui.column).lt(lit(ui.x));
+  if (value.type === 'lessThan') {
+    return col(value.column).lt(lit(value.x));
   }
 
-  if (ui.type === 'greaterThan') {
-    return col(ui.column).gt(lit(ui.x));
+  if (value.type === 'greaterThan') {
+    return col(value.column).gt(lit(value.x));
   }
 
-  if (ui.type === 'lessThanOrEqual') {
-    return col(ui.column).le(lit(ui.x));
+  if (value.type === 'lessThanOrEqual') {
+    return col(value.column).le(lit(value.x));
   }
 
-  if (ui.type === 'greaterThanOrEqual') {
-    return col(ui.column).ge(lit(ui.x));
+  if (value.type === 'greaterThanOrEqual') {
+    return col(value.column).ge(lit(value.x));
   }
 
-  if (ui.type === 'lessThanColumn') {
-    if (ui.minDiff !== undefined && ui.minDiff !== 0) {
-      return col(ui.column).plus(lit(ui.minDiff)).lt(col(ui.rhs));
+  if (value.type === 'lessThanColumn') {
+    if (value.minDiff !== undefined && value.minDiff !== 0) {
+      return col(value.column).plus(lit(value.minDiff)).lt(col(value.rhs));
     }
-    return col(ui.column).lt(col(ui.rhs));
+    return col(value.column).lt(col(value.rhs));
   }
 
-  if (ui.type === 'lessThanColumnOrEqual') {
-    if (ui.minDiff !== undefined && ui.minDiff !== 0) {
-      return col(ui.column).plus(lit(ui.minDiff)).le(col(ui.rhs));
+  if (value.type === 'lessThanColumnOrEqual') {
+    if (value.minDiff !== undefined && value.minDiff !== 0) {
+      return col(value.column).plus(lit(value.minDiff)).le(col(value.rhs));
     }
-    return col(ui.column).le(col(ui.rhs));
+    return col(value.column).le(col(value.rhs));
   }
 
-  if (ui.type === 'topN') {
-    return rank(col(ui.column), true).over([]).le(lit(ui.n));
+  if (value.type === 'topN') {
+    return rank(col(value.column), true).over([]).le(lit(value.n));
   }
 
-  if (ui.type === 'bottomN') {
-    return rank(col(ui.column), false).over([]).le(lit(ui.n));
+  if (value.type === 'bottomN') {
+    return rank(col(value.column), false).over([]).le(lit(value.n));
   }
 
-  if (ui.type === undefined) {
+  if (value.type === undefined) {
     throw new Error('Filter type is undefined, this should not happen');
   }
 
   throw new Error('Unhandled filter type');
 }
 
-export function convertFilterUiToExpressions(ui: FilterUi): Expression {
-  return convertFilterUiToExpressionImpl(ui).toJSON();
+export function convertFilterUiToExpressions(value: FilterUi): Expression {
+  return convertFilterUiToExpressionImpl(value).toJSON();
 }
