662eee0a1b852966e772d2a43c41e5be1c9e27dc
Vadim Piven <vadim.piven@milaboratories.com>
2025-09-10 14:59:05 +0200

    PFrames - spec update to support artificialColumn join entry (#1139)

--- lib/model/middle-layer/src/pframe/internal_api/api_read.ts
+++ lib/model/middle-layer/src/pframe/internal_api/api_read.ts
@@ -7,8 +7,10 @@ import {
   UniqueValuesRequest,
   UniqueValuesResponse
 } from '@milaboratories/pl-model-common';
-import { CreateTableRequestV3 } from './create_table';
+import { CreateTableRequestV3, CreateTableRequestV4 } from './create_table';
 import { PTableV7 } from './table';
+
+// TODO: remove...
 
 /** Read interface exposed by PFrames library */
 export interface PFrameReadAPIV9 {
@@ -36,6 +38,44 @@ export interface PFrameReadAPIV9 {
 
   /** Calculates data for the table and returns an object to access it */
   createTable(request: CreateTableRequestV3): PTableV7;
+
+  /** Calculate set of unique values for a specific axis for the filtered set of records */
+  getUniqueValues(
+    request: UniqueValuesRequest,
+    ops?: {
+      signal?: AbortSignal,
+    }
+  ): Promise<UniqueValuesResponse>;
+}
+
+// TODO: ...remove
+
+/** Read interface exposed by PFrames library */
+export interface PFrameReadAPIV10 {
+  /**
+   * Finds columns given filtering criteria on column name, annotations etc.
+   * and a set of qualified axes specs to find only columns with compatible
+   * axes spec.
+   *
+   * Only column specs are used, this method will work even for columns
+   * with no assigned data.
+   * */
+  findColumns(request: FindColumnsRequest): Promise<FindColumnsResponse>;
+
+  /**
+   * Construct new axes integration with some entry removed but integration qualificaitons preserved.
+   * Removes more then one entry in case the removal will create several disjoint sets of axes.
+   */
+  deleteColumn(request: DeleteColumnFromColumnsRequest): Promise<DeleteColumnFromColumnsResponse>;
+
+  /** Retrieve single column spec */
+  getColumnSpec(columnId: PObjectId): Promise<PColumnSpec>;
+
+  /** Retrieve information about all columns currently added to the PFrame */
+  listColumns(): Promise<PColumnInfo[]>;
+
+  /** Calculates data for the table and returns an object to access it */
+  createTable(request: CreateTableRequestV4): PTableV7;
 
   /** Calculate set of unique values for a specific axis for the filtered set of records */
   getUniqueValues(
