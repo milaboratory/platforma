72e488b9e7bc82300dff1cdafe27519926f0bdae
gramkin <bakluzhino@gmail.com>
2025-09-18 16:46:48 +0200

    first attempt

---
+++ lib/node/pl-client/src/core/advisory_lock.ts
@@ -0,0 +1,42 @@
+class AwaitLock {
+  private acquired = false;
+  private resolvers: (() => void)[] = [];
+
+  acquireAsync(): Promise<void> {
+    if (!this.acquired) {
+      this.acquired = true;
+      return Promise.resolve();
+    }
+
+    return new Promise((resolve) => {
+      this.resolvers.push(resolve);
+    });
+  }
+
+  release(): void {
+    if (!this.acquired) {
+      throw new Error('Cannot release an unacquired lock');
+    }
+
+    if (this.resolvers.length) {
+      this.resolvers.shift()?.();
+    } else {
+      this.acquired = false;
+    }
+  }
+}
+
+const m = new Map<string, AwaitLock>();
+
+export async function lock(id: string) {
+  if (!m.has(id)) {
+    const lock = new AwaitLock();
+    m.set(id, lock);
+  }
+
+  await m.get(id)!.acquireAsync();
+
+  return () => {
+    m.get(id)!.release();
+  };
+}
