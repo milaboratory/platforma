d3f972828b0c411a496fba203286508eda8f64c1
AStaroverov <hellbeast92@gmail.com>
2025-09-30 12:09:39 +0200

    feat: review fixes

--- tests/workflow-tengo/src/pt/frameFromBundle.test.ts
+++ tests/workflow-tengo/src/pt/frameFromBundle.test.ts
@@ -37,11 +37,23 @@ export async function readBlobAsString(driverKit: MiddleLayerDriverKit, handle: 
 tplTest(
   'frameFromColumnBundle: should return frame from column bundle with filtered columns',
   async ({ helper, expect, driverKit }) => {
-    const wf1 = await helper.renderWorkflow('pframes.test.frameFromBundle.pool', false, {}, { blockId: 'b1' });
+    const wf1 = await helper.renderWorkflow('pt.frameFromBundle.pool', false, {
+      sampleIdAxesSpec,
+      cellIdAxesSpec,
+      geneIdAxesSpec,
+      clusterResolutionSpec,
+      clusterResolutionCSV,
+      totalCountsSpec,
+      totalCountsCSV,
+      complexitySpec,
+      complexityCSV,
+      expressionSpec,
+      expressionCSV,
+    }, { blockId: 'b1' });
 
     const ctx = await awaitStableState(wf1.context());
 
-    const wf2 = await helper.renderWorkflow('pframes.test.frameFromBundle.exports', false, {
+    const wf2 = await helper.renderWorkflow('pt.frameFromBundle.exports', false, {
       columnIds: [
         canonicalizeJson<AnchoredPColumnId>({
           name: 'clusterResolution',
@@ -97,3 +109,129 @@ tplTest(
   },
   { concurrent: true },
 );
+
+const sampleIdAxesSpec = {
+  name: 'sampleId',
+  type: 'String',
+};
+
+const cellIdAxesSpec = {
+  name: 'cellId',
+  type: 'String',
+};
+
+const geneIdAxesSpec = {
+  name: 'geneId',
+  type: 'String',
+};
+
+const clusterResolutionSpec = {
+  kind: 'PColumn',
+  name: 'clusterResolution',
+  valueType: 'String',
+  axesSpec: [
+    sampleIdAxesSpec,
+  ],
+};
+const clusterResolutionCSV = `sampleId,clusterResolution
+1,CL-1
+2,CL-2
+3,CL-3`;
+
+const totalCountsSpec = {
+  kind: 'PColumn',
+  name: 'totalCounts',
+  valueType: 'Int',
+  axesSpec: [
+    sampleIdAxesSpec,
+    cellIdAxesSpec,
+  ],
+};
+const totalCountsCSV = `sampleId,cellId,totalCounts
+1,cell_1,3000
+1,cell_2,4000
+1,cell_3,3500
+2,cell_1,5000
+2,cell_2,6000
+2,cell_3,5500
+3,cell_1,7000
+3,cell_2,8000
+3,cell_3,7500`;
+
+const complexitySpec = {
+  kind: 'PColumn',
+  name: 'complexity',
+  valueType: 'Float',
+  axesSpec: [
+    sampleIdAxesSpec,
+    cellIdAxesSpec,
+    geneIdAxesSpec,
+  ],
+};
+const complexityCSV = `sampleId,cellId,geneId,complexity
+1,cell_1,gene_1,0.75
+1,cell_1,gene_2,0.710
+1,cell_1,gene_3,0.815
+1,cell_2,gene_1,0.87
+1,cell_2,gene_2,0.711
+1,cell_2,gene_3,0.513
+1,cell_3,gene_1,0.58
+1,cell_3,gene_2,0.712
+1,cell_3,gene_3,0.914
+2,cell_1,gene_1,0.920
+2,cell_1,gene_2,0.925
+2,cell_1,gene_3,0.730
+2,cell_2,gene_1,0.522
+2,cell_2,gene_2,0.227
+2,cell_2,gene_3,0.932
+2,cell_3,gene_1,0.324
+2,cell_3,gene_2,0.929
+2,cell_3,gene_3,0.934
+3,cell_1,gene_1,0.120
+3,cell_1,gene_2,0.125
+3,cell_1,gene_3,0.330
+3,cell_2,gene_1,0.122
+3,cell_2,gene_2,0.527
+3,cell_2,gene_3,0.132
+3,cell_3,gene_1,0.824
+3,cell_3,gene_2,0.629
+3,cell_3,gene_3,0.534`;
+
+const expressionSpec = {
+  kind: 'PColumn',
+  name: 'expression',
+  valueType: 'Float',
+  axesSpec: [
+    sampleIdAxesSpec,
+    cellIdAxesSpec,
+    geneIdAxesSpec,
+  ],
+};
+const expressionCSV = `sampleId,cellId,geneId,expression
+1,cell_1,gene_1,5
+1,cell_1,gene_2,10
+1,cell_1,gene_3,15
+1,cell_2,gene_1,7
+1,cell_2,gene_2,11
+1,cell_2,gene_3,13
+1,cell_3,gene_1,8
+1,cell_3,gene_2,12
+1,cell_3,gene_3,14
+2,cell_1,gene_1,20
+2,cell_1,gene_2,25
+2,cell_1,gene_3,30
+2,cell_2,gene_1,22
+2,cell_2,gene_2,27
+2,cell_2,gene_3,32
+2,cell_3,gene_1,24
+2,cell_3,gene_2,29
+2,cell_3,gene_3,34
+3,cell_1,gene_1,120
+3,cell_1,gene_2,125
+3,cell_1,gene_3,130
+3,cell_2,gene_1,122
+3,cell_2,gene_2,127
+3,cell_2,gene_3,132
+3,cell_3,gene_1,124
+3,cell_3,gene_2,129
+3,cell_3,gene_3,134`;
