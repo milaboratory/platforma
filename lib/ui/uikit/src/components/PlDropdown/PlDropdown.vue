1785beb9c6a27a936d42fafada15ee5c600b5b3f
Elena Erokhina <erohinaelena1@yandex.ru>
2025-09-30 17:35:08 +0200

    add 'position' for using dropdowns in 'stacks'

--- lib/ui/uikit/src/components/PlDropdown/PlDropdown.vue
+++ lib/ui/uikit/src/components/PlDropdown/PlDropdown.vue
@@ -61,9 +61,9 @@ const props = withDefaults(
      */
     error?: unknown;
     /**
-     * If false there is only red border with non-empty error field, without a message
+     * Shows red border even without an error message
      */
-    showErrorMessage?: boolean;
+    errorStatus?: boolean;
     /**
      * Placeholder text shown when no value is selected.
      */
@@ -92,6 +92,10 @@ const props = withDefaults(
      * Option list item size
      */
     optionSize?: 'small' | 'medium';
+    /**
+     * Makes some of corners not rounded
+     * */
+    position?: 'top' | 'bottom' | 'left' | 'right' | 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right' | 'middle';
   }>(),
   {
     label: '',
@@ -107,6 +111,7 @@ const props = withDefaults(
     arrowIconLarge: undefined,
     optionSize: 'small',
     options: undefined,
+    position: undefined,
   },
 );
 
@@ -147,10 +152,6 @@ const selectedIndex = computed(() => {
 });
 
 const computedError = computed(() => {
-  if (!props.showErrorMessage) {
-    return undefined;
-  }
-
   if (isLoadingOptions.value) {
     return undefined;
   }
@@ -325,7 +326,7 @@ watchPostEffect(() => {
       ref="rootRef"
       :tabindex="tabindex"
       class="pl-dropdown"
-      :class="{ open: data.open, error, disabled: isDisabled }"
+      :class="{ open: data.open, error: error || errorStatus, disabled: isDisabled }"
       @keydown="handleKeydown"
       @focusout="onFocusOut"
     >
@@ -376,7 +377,7 @@ watchPostEffect(() => {
           :option-size="optionSize"
           :select-option="selectOptionWrapper"
         />
-        <DoubleContour class="pl-dropdown__contour" />
+        <DoubleContour class="pl-dropdown__contour" :position="position" />
       </div>
     </div>
     <div v-if="computedError" class="pl-dropdown__error">{{ computedError }}</div>
