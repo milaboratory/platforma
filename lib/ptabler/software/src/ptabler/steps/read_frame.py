aa004dac0e3c8f2d2c2685d8c70bd9561465c66c
Vadim Piven <vadim.piven@milaboratories.com>
2025-10-03 16:51:51 +0200

    Translate column names

--- lib/ptabler/software/src/ptabler/steps/read_frame.py
+++ lib/ptabler/software/src/ptabler/steps/read_frame.py
@@ -1,9 +1,10 @@
 import os
+from typing import Mapping
 
 import polars as pl
 import polars_pf as ppf
 from polars._typing import ParallelStrategy
-from polars_pf import CreateTableRequest
+from polars_pf import axis_ref, CreateTableRequest, PTableColumnSpec, PTableColumnSpecAxis
 
 from .base import PStep, StepContext
 
@@ -17,10 +18,17 @@ class ReadFrame(PStep, tag="read_frame"):
     """Name to assign to the loaded DataFrame in the tablespace"""
     request: CreateTableRequest
     """Request to create the table"""
+    translation: Mapping[str, str]
+    """Translation of PFrame column ids into Polars column names"""
     parallel: ParallelStrategy = "auto"
     """Parallel strategy to use for the read"""
     low_memory: bool = False
     """Whether to use low memory mode for the read"""
+
+    def column_ref(self, spec: PTableColumnSpec) -> str:
+        if isinstance(spec, PTableColumnSpecAxis):
+            return axis_ref(spec.spec)
+        return self.translation[spec.id]
 
     def execute(self, ctx: StepContext) -> None:
         if not os.path.isdir(ctx.settings.frame_folder):
@@ -29,8 +37,9 @@ class ReadFrame(PStep, tag="read_frame"):
         lf: pl.LazyFrame = ppf.pframe_source(
             ctx.settings.frame_folder,
             self.request,
-            logger=ppf.logger,
             spill_path=ctx.settings.spill_folder,
+            column_ref=self.column_ref,
+            logger=ppf.logger,
             parallel=self.parallel,
             low_memory=self.low_memory,
         )
