a9517a87c1ddb95108938a09bd252cac3ec94ae0
Kevin Detry <206546615+kevindetry-milaboratories@users.noreply.github.com>
2025-10-07 11:20:20 +0200

    Add HTTP(S) proxy support in built-in mode (#1176)

---
+++ .changeset/four-planets-lick.md
@@ -0,0 +1,7 @@
+---
+'@milaboratories/pl-deployments': minor
+'@milaboratories/pl-client': minor
+'@milaboratories/pl-http': minor
+---
+
+Add HTTP(S) proxy support in built-in mode
--- lib/node/pl-http/src/index.ts
+++ lib/node/pl-http/src/index.ts
@@ -1,9 +1,19 @@
 import type { Client, Dispatcher } from 'undici';
 import { Agent, ProxyAgent, interceptors } from 'undici';
 
-export function defaultHttpDispatcher(httpProxy?: string): Dispatcher {
+export type ProxySettings = {
+  url?: string;
+  /**
+   * @see {@link https://developer.mozilla.org/en-US/docs/Web/HTTP/Reference/Headers/Proxy-Authorization}.
+   */
+  auth?: string;
+};
+
+export function defaultHttpDispatcher(
+  httpProxy?: string | ProxySettings,
+): Dispatcher {
   const httpOptions: Client.Options = {
-    allowH2: true,
+    // allowH2: true, // Turning this on makes downloads almost 10x as slow
     autoSelectFamily: false,
     headersTimeout: 15e3,
     bodyTimeout: 30e3, // Reset connection after 30 seconds of inactivity, better retry
@@ -11,16 +21,17 @@ export function defaultHttpDispatcher(httpProxy?: string): Dispatcher {
     keepAliveMaxTimeout: 60e3,
   };
 
-  const dispatcher = httpProxy !== undefined
-    ? new ProxyAgent({ uri: httpProxy, ...httpOptions })
-    : new Agent(httpOptions);
+  const proxy = typeof httpProxy === 'string' ? { url: httpProxy } : httpProxy;
 
-  return dispatcher
-    .compose(
-      interceptors.dns({
-        maxTTL: 60e3, // Cache DNS results for 1 minute (default: 10 seconds)
-        affinity: 4,
-      }),
-      interceptors.retry(),
-    );
+  const dispatcher = proxy?.url
+    ? new ProxyAgent({ uri: proxy.url, token: proxy.auth, ...httpOptions })
+    : new Agent(httpOptions)
+      .compose(
+        interceptors.dns({
+          maxTTL: 60e3, // Cache DNS results for 1 minute (default: 10 seconds)
+          affinity: 4,
+        }),
+      );
+
+  return dispatcher.compose(interceptors.retry());
 }
