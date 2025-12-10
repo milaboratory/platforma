d57fb3e3ceba0c0580590abd8b1c1410a96c0d8c
Alexandr <hellbeast92@gmail.com>
2025-12-10 16:15:33 +0100

    feat: export PlAdvancedFilterColumnId (#1322)

--- sdk/ui-vue/src/components/PlAdvancedFilter/index.ts
+++ sdk/ui-vue/src/components/PlAdvancedFilter/index.ts
@@ -1,7 +1,8 @@
 import { SUPPORTED_FILTER_TYPES } from './constants.ts';
-import type { RootFilter, SourceOptionInfo } from './types.ts';
+import type { RootFilter, SourceOptionInfo, PlAdvancedFilterColumnId } from './types.ts';
 
 export { default as PlAdvancedFilter } from './PlAdvancedFilter.vue';
 export const PlAdvancedFilterSupportedFilters = SUPPORTED_FILTER_TYPES;
 export type PlAdvancedFilterItem = SourceOptionInfo;
 export type PlAdvancedFilterFilter = RootFilter;
+export { PlAdvancedFilterColumnId };
