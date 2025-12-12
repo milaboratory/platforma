b96330dea2b72fc59994986e9b08a8b3c7b231cb
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2025-12-12 10:31:04 +0100

    refactor: rename parameter in sdkPlugin definition for clarity

--- etc/blocks/pool-explorer/ui/src/app.ts
+++ etc/blocks/pool-explorer/ui/src/app.ts
@@ -2,7 +2,7 @@ import { platforma } from '@milaboratories/milaboratories.pool-explorer.model';
 import MainPage from './MainPage.vue';
 import { defineApp } from '@platforma-sdk/ui-vue';
 
-export const sdkPlugin = defineApp(platforma, (base) => {
+export const sdkPlugin = defineApp(platforma, (_base) => {
   return {
     routes: {
       '/': () => MainPage,
