f92337580433060f6cc387293492ef0840dd5627
AStaroverov <hellbeast92@gmail.com>
2026-02-17 11:16:55 +0100

    sort query for reusage joins

--- lib/node/pf-driver/src/driver_impl.ts
+++ lib/node/pf-driver/src/driver_impl.ts
@@ -30,6 +30,8 @@ import {
   type PTableRecordFilter,
   type JsonSerializable,
   type PTableDefV2,
+  type QuerySpec,
+  type QueryJoinEntrySpec,
 } from "@platforma-sdk/model";
 import type { PFrameInternal } from "@milaboratories/pl-model-middle-layer";
 import {
@@ -60,7 +62,6 @@ import {
 } from "./ptable_cache_plain";
 import { createPFrame as createSpecFrame } from "@milaboratories/pframes-rs-wasm";
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface LocalBlobProvider<
   TreeEntry extends JsonSerializable,
 > extends PoolLocalBlobProvider<TreeEntry> {}
@@ -216,7 +217,8 @@ export class AbstractPFrameDriver<
 
     const pFrameEntry = this.createPFrame(columns);
     const specFrame = createSpecFrame(columnsMap);
-    const { tableSpec, dataQuery } = specFrame.evaluateQuery(def.query);
+    const sortedQuery = sortQuerySpec(def.query);
+    const { tableSpec, dataQuery } = specFrame.evaluateQuery(sortedQuery);
 
     const pTableEntry = this.pTableDefs.acquire({
       type: "v2",
@@ -558,6 +560,148 @@ function sortPTableDef(def: PTableDef<PObjectId>): PTableDef<PObjectId> {
     filters: sortFilters(def.filters),
     sorting: def.sorting,
   };
+}
+
+function cmpQueryJoinEntrySpec(lhs: QueryJoinEntrySpec, rhs: QueryJoinEntrySpec): number {
+  const cmp = cmpQuerySpec(lhs.entry, rhs.entry);
+  if (cmp !== 0) return cmp;
+  if (lhs.qualifications.length !== rhs.qualifications.length) {
+    return lhs.qualifications.length - rhs.qualifications.length;
+  }
+  for (let i = 0; i < lhs.qualifications.length; i++) {
+    const lhsQ = canonicalizeJson(lhs.qualifications[i]);
+    const rhsQ = canonicalizeJson(rhs.qualifications[i]);
+    if (lhsQ !== rhsQ) return lhsQ < rhsQ ? -1 : 1;
+  }
+  return 0;
+}
+
+function cmpQuerySpec(lhs: QuerySpec, rhs: QuerySpec): number {
+  if (lhs.type !== rhs.type) {
+    return lhs.type < rhs.type ? -1 : 1;
+  }
+  switch (lhs.type) {
+    case "column":
+      return lhs.columnId < (rhs as typeof lhs).columnId
+        ? -1
+        : lhs.columnId === (rhs as typeof lhs).columnId
+          ? 0
+          : 1;
+    case "inlineColumn":
+      return lhs.spec.id < (rhs as typeof lhs).spec.id
+        ? -1
+        : lhs.spec.id === (rhs as typeof lhs).spec.id
+          ? 0
+          : 1;
+    case "sparseToDenseColumn":
+      return lhs.columnId < (rhs as typeof lhs).columnId
+        ? -1
+        : lhs.columnId === (rhs as typeof lhs).columnId
+          ? 0
+          : 1;
+    case "innerJoin":
+    case "fullJoin": {
+      const rhsJoin = rhs as typeof lhs;
+      if (lhs.entries.length !== rhsJoin.entries.length) {
+        return lhs.entries.length - rhsJoin.entries.length;
+      }
+      for (let i = 0; i < lhs.entries.length; i++) {
+        const cmp = cmpQueryJoinEntrySpec(lhs.entries[i], rhsJoin.entries[i]);
+        if (cmp !== 0) return cmp;
+      }
+      return 0;
+    }
+    case "outerJoin": {
+      const rhsOuter = rhs as typeof lhs;
+      const cmp = cmpQueryJoinEntrySpec(lhs.primary, rhsOuter.primary);
+      if (cmp !== 0) return cmp;
+      if (lhs.secondary.length !== rhsOuter.secondary.length) {
+        return lhs.secondary.length - rhsOuter.secondary.length;
+      }
+      for (let i = 0; i < lhs.secondary.length; i++) {
+        const cmp = cmpQueryJoinEntrySpec(lhs.secondary[i], rhsOuter.secondary[i]);
+        if (cmp !== 0) return cmp;
+      }
+      return 0;
+    }
+    case "sliceAxes":
+      return cmpQuerySpec(lhs.input, (rhs as typeof lhs).input);
+    case "sort":
+      return cmpQuerySpec(lhs.input, (rhs as typeof lhs).input);
+    case "filter":
+      return cmpQuerySpec(lhs.input, (rhs as typeof lhs).input);
+    default:
+      assertNever(lhs);
+  }
+}
+
+function sortQueryJoinEntrySpec(entry: QueryJoinEntrySpec): QueryJoinEntrySpec {
+  const sortedQualifications = entry.qualifications.toSorted((lhs, rhs) => {
+    const lhsKey = canonicalizeJson(lhs.axis);
+    const rhsKey = canonicalizeJson(rhs.axis);
+    return lhsKey < rhsKey ? -1 : lhsKey === rhsKey ? 0 : 1;
+  });
+  return {
+    entry: sortQuerySpec(entry.entry),
+    qualifications: sortedQualifications,
+  };
+}
+
+function sortQuerySpec(query: QuerySpec): QuerySpec {
+  switch (query.type) {
+    case "column":
+    case "inlineColumn":
+      return query;
+    case "sparseToDenseColumn": {
+      const sortedAxesIndices = query.axesIndices.toSorted((lhs, rhs) => lhs - rhs);
+      return {
+        ...query,
+        axesIndices: sortedAxesIndices,
+      };
+    }
+    case "innerJoin":
+    case "fullJoin": {
+      const sortedEntries = query.entries.map(sortQueryJoinEntrySpec);
+      sortedEntries.sort(cmpQueryJoinEntrySpec);
+      return {
+        ...query,
+        entries: sortedEntries,
+      };
+    }
+    case "outerJoin": {
+      const sortedSecondary = query.secondary.map(sortQueryJoinEntrySpec);
+      sortedSecondary.sort(cmpQueryJoinEntrySpec);
+      return {
+        ...query,
+        primary: sortQueryJoinEntrySpec(query.primary),
+        secondary: sortedSecondary,
+      };
+    }
+    case "sliceAxes": {
+      const sortedAxisFilters = query.axisFilters.toSorted((lhs, rhs) => {
+        const lhsKey = canonicalizeJson(lhs.axisSelector);
+        const rhsKey = canonicalizeJson(rhs.axisSelector);
+        return lhsKey < rhsKey ? -1 : lhsKey === rhsKey ? 0 : 1;
+      });
+      return {
+        ...query,
+        input: sortQuerySpec(query.input),
+        axisFilters: sortedAxisFilters,
+      };
+    }
+    case "sort":
+      return {
+        ...query,
+        input: sortQuerySpec(query.input),
+      };
+    case "filter":
+      return {
+        ...query,
+        input: sortQuerySpec(query.input),
+      };
+    default:
+      assertNever(query);
+  }
 }
 
 function migrateFilters(
