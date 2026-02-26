f20b5e08963a914b8bcd56479320b7e738a3b41d
AStaroverov <hellbeast92@gmail.com>
2026-02-26 19:13:53 +0100

    remove deprecated code

--- etc/ui-vue-docs/.vitepress/docs-components/btn-group/examples/index.ts
+++
@@ -1,7 +0,0 @@
-import BtnGroupBasic from "./BtnGroupBasic.vue";
-import BtnGroupBasicRaw from "./BtnGroupBasic.vue?raw";
-
-import BtnGroupLabel from "./BtnGroupLabel.vue";
-import BtnGroupLabelRaw from "./BtnGroupLabel.vue?raw";
-
-export { BtnGroupBasic, BtnGroupBasicRaw, BtnGroupLabel, BtnGroupLabelRaw };
