d1e603bc23c8b363c6251ca32a3f4061fc5807ad
Vadim Piven <vadim.piven@milaboratories.com>
2026-03-23 17:02:12 +0100

    Remove pf-spec-driver dependency and re-export from pf-driver

--- lib/node/pf-driver/src/index.ts
+++ lib/node/pf-driver/src/index.ts
@@ -2,4 +2,3 @@ export * from "./driver_decl";
 export * from "./driver_impl";
 export * from "./data_info_helpers";
 export * from "./driver_double";
-export * from "@milaboratories/pf-spec-driver";
