93e4ab0c28ff74afd18fc2a608e8f41ccd1a2a46
AStaroverov <hellbeast92@gmail.com>
2026-04-10 12:52:16 +0200

    fix: correct export name for discoverTableColumnSnapshots in PlDataTable index

--- sdk/model/src/components/PlDataTable/index.ts
+++ sdk/model/src/components/PlDataTable/index.ts
@@ -22,7 +22,7 @@ export {
 } from "./state-migration";
 
 export { createPlDataTableSheet } from "./createPlDataTableSheet";
-export { createPlDataTable, discoverColumns } from "./createPlDataTable";
+export { createPlDataTable, discoverTableColumnSnaphots } from "./createPlDataTable";
 export { createPlDataTableV2 } from "./createPlDataTable/createPlDataTableV2";
 export { createPlDataTableV3 } from "./createPlDataTable/createPlDataTableV3";
 export {
