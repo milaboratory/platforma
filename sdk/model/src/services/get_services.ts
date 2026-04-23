1420d8b076f9e5dcfd2f5bbd10839626e70b8f53
AStaroverov <hellbeast92@gmail.com>
2026-04-23 15:25:30 +0200

    refactor: optimize service caching mechanism using WeakMap for context-based storage

--- sdk/model/src/services/get_services.ts
+++ sdk/model/src/services/get_services.ts
@@ -2,15 +2,27 @@ import { ModelServices, ServiceName } from "@milaboratories/pl-model-common";
 import { getCfgRenderCtx } from "../internal";
 import { ValueOf } from "@milaboratories/helpers";
 import { createServiceProxy } from "./service_bridge";
+import { GlobalCfgRenderCtx } from "../render/internal";
 
-let cachedServices = new Map<keyof ModelServices, ValueOf<ModelServices>>();
+const cachedServices = new WeakMap<
+  GlobalCfgRenderCtx,
+  Map<keyof ModelServices, ValueOf<ModelServices>>
+>();
 
 export function getService<T extends keyof ModelServices>(name: T): ModelServices[T] {
-  if (!cachedServices.has(name)) {
-    const id = name as ServiceName;
-    const ctx = getCfgRenderCtx();
-    cachedServices.set(name, createServiceProxy(ctx)(id) as ModelServices[T]);
-  }
+  const ctx = getCfgRenderCtx();
 
-  return cachedServices.get(name) as ModelServices[T];
+  const map = cachedServices.has(ctx)
+    ? cachedServices.get(ctx)!
+    : (() => {
+        cachedServices.set(ctx, new Map());
+        return cachedServices.get(ctx)!;
+      })();
+
+  return map.has(name)
+    ? (map.get(name) as ModelServices[T])
+    : (() => {
+        map.set(name, createServiceProxy(ctx)(name as ServiceName) as ModelServices[T]);
+        return map.get(name) as ModelServices[T];
+      })();
 }
