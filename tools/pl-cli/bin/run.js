7ca2af755b09f59ad12fece6e83af1b90e166401
Korenevskiy Denis <denkoren@gmail.com>
2026-04-28 11:47:53 +0300

    chore: make pl-cli tool to not spam with warnings in dev mode

--- tools/pl-cli/bin/run.js
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-import { execute } from "@oclif/core";
-import { dirname } from "node:path";
-import { fileURLToPath } from "node:url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-await execute({ dir: __dirname });
