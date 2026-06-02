40c9195cfd7e6af75332456369df7527bd310121
Dmitry Bolotin <bolotin.dmitriy@gmail.com>
2026-06-02 23:27:11 +0200

    chore(block-tools-structure): regenerate in-mono example blocks

--- etc/blocks/transfer-files/block/index.js
+++ etc/blocks/transfer-files/block/index.js
@@ -1,15 +1,8 @@
-const blockTools = require('@platforma-sdk/block-tools');
-
-async function loadBlockDescription() {
-  return await blockTools.loadPackDescriptionFromSource(__dirname);
-}
-
 const blockSpec = {
-  type: 'dev-v2',
-  folder: __dirname
+  type: "dev-v2",
+  folder: __dirname,
 };
 
 module.exports = {
   blockSpec,
-  loadBlockDescription
 };
